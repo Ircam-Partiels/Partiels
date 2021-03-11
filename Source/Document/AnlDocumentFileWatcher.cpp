7d990eb6cba3d0c3ad0a3fc6e60a650bffbd6db8
Pierre Guillot <guillotpierre6@gmail.com>
2021-03-11 19:01:31 +0100

    Document::FileWatcher: fix

--- Source/Document/AnlDocumentFileWatcher.cpp
+++ Source/Document/AnlDocumentFileWatcher.cpp
@@ -45,6 +45,7 @@ void Document::FileWatcher::timerCallback()
     auto const file = mAccessor.getAttr<AttrType::file>();
     if(!file.existsAsFile())
     {
+        stopTimer();
         auto constexpr icon = juce::AlertWindow::AlertIconType::WarningIcon;
         auto const title = juce::translate("Audio file has been moved or deleted!");
         auto const message = juce::translate("The audio file FLNM has been moved or deleted. Would you like to restore  it?").replace("FLNM", file.getFullPathName());
@@ -58,17 +59,22 @@ void Document::FileWatcher::timerCallback()
             }
             mAccessor.setAttr<AttrType::file>(fc.getResult(), NotificationType::synchronous);
         }
+        startTimer(200);
     }
     auto const time = file.getLastModificationTime();
-    if(time > mModificationTime)
+    if(time != mModificationTime)
     {
+        mModificationTime = time;
+        stopTimer();
         auto constexpr icon = juce::AlertWindow::AlertIconType::WarningIcon;
         auto const title = juce::translate("Audio file  has been modified!");
         auto const message = juce::translate("The audio file FLNM has been modified. Would you like to reload it?").replace("FLNM", file.getFullPathName());
         if(juce::AlertWindow::showOkCancelBox(icon, title, message))
         {
+            mAccessor.setAttr<AttrType::file>(juce::File{}, NotificationType::synchronous);
             mAccessor.setAttr<AttrType::file>(file, NotificationType::synchronous);
         }
+        startTimer(200);
     }
 }
 
