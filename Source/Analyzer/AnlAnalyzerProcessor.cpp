29061657c264790cf7612b54bc2edf531d2031d6
Pierre Guillot <guillotpierre6@gmail.com>
2021-03-18 18:21:27 +0100

    add Analyzer Processor

---
+++ Source/Analyzer/AnlAnalyzerProcessor.cpp
@@ -0,0 +1,148 @@
+#include "AnlAnalyzerProcessor.h"
+#include "../Plugin/AnlPluginProcessor.h"
+
+ANALYSE_FILE_BEGIN
+
+Analyzer::Processor::~Processor()
+{
+    std::unique_lock<std::mutex> lock(mAnalysisMutex);
+    abortAnalysis();
+}
+
+void Analyzer::Processor::stopAnalysis()
+{
+    std::unique_lock<std::mutex> lock(mAnalysisMutex);
+    abortAnalysis();
+}
+
+Analyzer::Processor::Result Analyzer::Processor::runAnalysis(Accessor const& accessor, juce::AudioFormatReader& reader)
+{
+    auto const& description = accessor.getAttr<AttrType::description>();
+    auto const& state = accessor.getAttr<AttrType::state>();
+    
+    std::unique_lock<std::mutex> lock(mAnalysisMutex, std::try_to_lock);
+    anlStrongAssert(lock.owns_lock());
+    if(!lock.owns_lock())
+    {
+        return {};
+    }
+    abortAnalysis();
+    
+    auto const key = accessor.getAttr<AttrType::key>();
+    if(key.identifier.empty() || key.feature.empty())
+    {
+        return {};
+    }
+    
+    anlStrongAssert(state.blockSize > 0 && state.stepSize > 0);
+    if(state.blockSize == 0 || state.stepSize == 0)
+    {
+        return {};
+    }
+    
+    std::unique_ptr<Plugin::Processor> processor;
+    try
+    {
+        processor = Plugin::Processor::create(key, state, reader);
+    }
+    catch(std::exception& e)
+    {
+        return std::make_tuple(WarningType::plugin, e.what(), description, state);
+    }
+    catch(...)
+    {
+        return std::make_tuple(WarningType::plugin, "unknown error", description, state);
+    }
+    
+    if(processor == nullptr)
+    {
+        return std::make_tuple(WarningType::state, "invalid state", description, state);
+    }
+    
+    auto const pluginDescription = processor->getDescription();
+    anlStrongAssert(pluginDescription != Plugin::Description{});
+    if(pluginDescription == Plugin::Description{})
+    {
+        return std::make_tuple(WarningType::plugin, "invalid description", pluginDescription, state);
+    }
+    
+    mChrono.start();
+    mAnalysisProcess = std::async([this, processor = std::move(processor)]() -> std::vector<Plugin::Result>
+    {
+        juce::Thread::setCurrentThreadName("Analyzer::Director::runAnalysis");
+        juce::Thread::setCurrentThreadPriority(10);
+        
+        auto expected = ProcessState::available;
+        if(!mAnalysisState.compare_exchange_weak(expected, ProcessState::running))
+        {
+            triggerAsyncUpdate();
+            return {};
+        }
+        expected = ProcessState::running;
+
+        std::vector<Plugin::Result> results;
+        if(!processor->prepareToAnalyze(results))
+        {
+            mAnalysisState.compare_exchange_weak(expected, ProcessState::aborted);
+            triggerAsyncUpdate();
+            return {};
+        }
+        
+        while(mAnalysisState.load() != ProcessState::aborted && processor->performNextAudioBlock(results))
+        {
+        }
+        
+        if(mAnalysisState.compare_exchange_weak(expected, ProcessState::ended))
+        {
+            triggerAsyncUpdate();
+            return results;
+        }
+        
+        triggerAsyncUpdate();
+        return {};
+    });
+    
+    return std::make_tuple(WarningType::none, "", description, state);
+}
+
+void Analyzer::Processor::handleAsyncUpdate()
+{
+    std::unique_lock<std::mutex> lock(mAnalysisMutex);
+    if(mAnalysisProcess.valid())
+    {
+        anlWeakAssert(mAnalysisState != ProcessState::available);
+        anlWeakAssert(mAnalysisState != ProcessState::running);
+        
+        auto const results = mAnalysisProcess.get();
+        auto expected = ProcessState::ended;
+        if(mAnalysisState.compare_exchange_weak(expected, ProcessState::available))
+        {
+            mChrono.stop();
+            if(onAnalysisEnded != nullptr)
+            {
+                onAnalysisEnded(results);
+            }
+        }
+        else if(expected == ProcessState::aborted)
+        {
+            if(onAnalysisAborted != nullptr)
+            {
+                onAnalysisAborted();
+            }
+        }
+        abortAnalysis();
+    }
+}
+
+void Analyzer::Processor::abortAnalysis()
+{
+    if(mAnalysisProcess.valid())
+    {
+        mAnalysisState = ProcessState::aborted;
+        mAnalysisProcess.get();
+        cancelPendingUpdate();
+    }
+    mAnalysisState = ProcessState::available;
+}
+
+ANALYSE_FILE_END
---
+++ Source/Analyzer/AnlAnalyzerProcessor.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "AnlAnalyzerModel.h"
+#include "../Plugin/AnlPluginListScanner.h"
+
+ANALYSE_FILE_BEGIN
+
+namespace Analyzer
+{
+    class Processor
+    : private juce::AsyncUpdater
+    {
+    public:
+        using Result = std::optional<std::tuple<WarningType, juce::String, Plugin::Description, Plugin::State>>;
+        
+        Processor() = default;
+        ~Processor() override;
+        
+        Result runAnalysis(Accessor const& accessor, juce::AudioFormatReader& reader);
+        void stopAnalysis();
+        
+        std::function<void(std::vector<Plugin::Result> const& results)> onAnalysisEnded = nullptr;
+        std::function<void(void)> onAnalysisAborted = nullptr;
+        
+    private:
+        
+        void abortAnalysis();
+        
+        // juce::AsyncUpdater
+        void handleAsyncUpdate() override;
+       
+        enum class ProcessState
+        {
+              available
+            , aborted
+            , running
+            , ended
+        };
+        
+        std::unique_ptr<juce::AudioFormatReader> mAudioFormatReaderManager;
+        
+        std::atomic<ProcessState> mAnalysisState {ProcessState::available};
+        std::future<std::vector<Plugin::Result>> mAnalysisProcess;
+        std::mutex mAnalysisMutex;
+        Chrono mChrono {"Analyzer", "processor analysis ended"};
+
+        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Processor)
+    };
+}
+
+ANALYSE_FILE_END
