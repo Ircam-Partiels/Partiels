2fab55919586ad5ecf9b0e6b833d54fafa440fa3
Pierre Guillot <guillotpierre6@gmail.com>
2021-04-07 12:08:45 +0200

    Document::GroupPlot::Overlay: add class

--- Source/Document/AnlDocumentGroupPlot.cpp
+++ Source/Document/AnlDocumentGroupPlot.cpp
@@ -1,4 +1,6 @@
 #include "AnlDocumentGroupPlot.h"
+#include "../Zoom/AnlZoomTools.h"
+#include "../Track/AnlTrackTools.h"
 
 ANALYSE_FILE_BEGIN
 
@@ -122,6 +124,74 @@ void Document::GroupPlot::resized()
             it->second->setBounds(bounds);
         }
     }
+}
+
+Document::GroupPlot::Overlay::Overlay(GroupPlot& groupPlot)
+: mGroupPlot(groupPlot)
+, mAccessor(mGroupPlot.mAccessor)
+, mTransportPlayheadContainer(mAccessor.getAcsr<AcsrType::transport>(), mAccessor.getAcsr<AcsrType::timeZoom>())
+{
+    addAndMakeVisible(mGroupPlot);
+    mTooltip.setEditable(false);
+    mTooltip.setJustificationType(juce::Justification::topLeft);
+    mTooltip.setInterceptsMouseClicks(false, false);
+    mTooltip.setComponentEffect(&mDropShadowEffect);
+    addChildComponent(mTooltip);
+    addAndMakeVisible(mTransportPlayheadContainer);
+    mTransportPlayheadContainer.setInterceptsMouseClicks(false, false);
+    addMouseListener(&mTransportPlayheadContainer, false);
+    setInterceptsMouseClicks(true, true);
+}
+
+void Document::GroupPlot::Overlay::resized()
+{
+    auto const bounds = getLocalBounds();
+    mGroupPlot.setBounds(bounds);
+    mTransportPlayheadContainer.setBounds(bounds);
+    mTooltip.setBounds(0, 0, 200, getHeight());
+}
+
+void Document::GroupPlot::Overlay::mouseMove(juce::MouseEvent const& event)
+{
+    auto const time = Zoom::Tools::getScaledValueFromWidth(mAccessor.getAcsr<AcsrType::timeZoom>(), *this, event.x);
+    
+    auto const& tracks = mAccessor.getAcsrs<AcsrType::tracks>();
+    auto const& layout = mAccessor.getAttr<AttrType::layout>();
+
+    auto text = Format::secondsToString(time);
+    for(auto it = layout.crbegin(); it != layout.crend(); ++it)
+    {
+        auto const identifier = *it;
+        auto trackIt = std::find_if(tracks.cbegin(), tracks.cend(), [&](auto const& trackAcsr)
+        {
+            return trackAcsr.get().template getAttr<Track::AttrType::identifier>() == identifier;
+        });
+        if(trackIt != tracks.cend())
+        {
+            auto const& trackAcsr = trackIt->get();
+            auto const name = trackAcsr.getAttr<Track::AttrType::name>();
+            
+            auto const bin = Zoom::Tools::getScaledValueFromHeight(trackAcsr.getAcsr<Track::AcsrType::binZoom>(), *this, event.y);
+            auto const tip = Format::secondsToString(time) + " • "+ Track::Tools::getResultText(trackAcsr, time, static_cast<size_t>(std::floor(bin)));
+            text += "\n" + tip;
+        }
+    }
+
+//    auto const tip = Format::secondsToString(time) + " • "+  getTooltip();
+//    setTooltip(name + ": " + tip);
+    mTooltip.setText(text, juce::NotificationType::dontSendNotification);
+}
+
+void Document::GroupPlot::Overlay::mouseEnter(juce::MouseEvent const& event)
+{
+    mTooltip.setVisible(true);
+    mouseMove(event);
+}
+
+void Document::GroupPlot::Overlay::mouseExit(juce::MouseEvent const& event)
+{
+    juce::ignoreUnused(event);
+    mTooltip.setVisible(false);
 }
 
 ANALYSE_FILE_END
--- Source/Document/AnlDocumentGroupPlot.h
+++ Source/Document/AnlDocumentGroupPlot.h
@@ -18,6 +18,28 @@ namespace Document
         // juce::Component
         void resized() override;
         
+        class Overlay
+        : public juce::Component
+        , public juce::SettableTooltipClient
+        {
+        public:
+            Overlay(GroupPlot& groupPlot);
+            ~Overlay() override = default;
+            
+            // juce::Component
+            void resized() override;
+            void mouseMove(juce::MouseEvent const& event) override;
+            void mouseEnter(juce::MouseEvent const& event) override;
+            void mouseExit(juce::MouseEvent const& event) override;
+            
+        private:
+            GroupPlot& mGroupPlot;
+            Accessor& mAccessor;
+            Transport::PlayheadContainer mTransportPlayheadContainer;
+            juce::Label mTooltip;
+            juce::DropShadowEffect mDropShadowEffect;
+        };
+        
     private:
         Accessor& mAccessor;
         Accessor::Listener mListener;
