b58733771d9a3ccdcf63ca4bcb5e0b31c8ebedcd
Pierre Guillot <guillotpierre6@gmail.com>
2021-04-07 12:06:13 +0200

    Zoom::Tools: add getScaledValueFromWidth(), getScaledValueFromHeight(), getScaledXFromValue() & getScaledYFromValue()

--- Source/Zoom/AnlZoomTools.cpp
+++ Source/Zoom/AnlZoomTools.cpp
@@ -2,7 +2,7 @@
 
 ANALYSE_FILE_BEGIN
 
-Zoom::Range Zoom::Tools::getScaledVisibleRange(Zoom::Accessor const& acsr, Zoom::Range const& newGlobalRange)
+Zoom::Range Zoom::Tools::getScaledVisibleRange(Accessor const& acsr, Range const& newGlobalRange)
 {
     auto const globalRange = acsr.getAttr<AttrType::globalRange>();
     if(globalRange.isEmpty())
@@ -15,6 +15,50 @@ Zoom::Range Zoom::Tools::getScaledVisibleRange(Zoom::Accessor const& acsr, Zoom:
     auto const newStart = startRatio * newGlobalRange.getLength() + newGlobalRange.getStart();
     auto const newEnd = endRatio * newGlobalRange.getLength() + newGlobalRange.getStart();
     return {newStart, newEnd};
+}
+
+double Zoom::Tools::getScaledValueFromWidth(Accessor const& zoomAcsr, juce::Component const& component, int x)
+{
+    auto const range = zoomAcsr.getAttr<Zoom::AttrType::visibleRange>();
+    auto const width = component.getWidth();
+    if(width <= 0 || range.isEmpty())
+    {
+        return 0.0;
+    }
+    return static_cast<double>(x) / static_cast<double>(width) * range.getLength() + range.getStart();
+}
+
+double Zoom::Tools::getScaledValueFromHeight(Accessor const& zoomAcsr, juce::Component const& component, int y)
+{
+    auto const range = zoomAcsr.getAttr<Zoom::AttrType::visibleRange>();
+    auto const height = component.getHeight();
+    if(height <= 0 || range.isEmpty())
+    {
+        return 0.0;
+    }
+    return static_cast<double>((height - 1) - y) / static_cast<double>(height) * range.getLength() + range.getStart();
+}
+
+int Zoom::Tools::getScaledXFromValue(Accessor const& zoomAcsr, juce::Component const& component, double value)
+{
+    auto const range = zoomAcsr.getAttr<Zoom::AttrType::visibleRange>();
+    auto const width = component.getWidth();
+    if(width <= 0 || range.isEmpty())
+    {
+        return 0;
+    }
+    return static_cast<int>(std::round((value - range.getStart()) / range.getLength() * static_cast<double>(width)));
+}
+
+int Zoom::Tools::getScaledYFromValue(Accessor const& zoomAcsr, juce::Component const& component, double value)
+{
+    auto const range = zoomAcsr.getAttr<Zoom::AttrType::visibleRange>();
+    auto const height = component.getHeight();
+    if(height <= 0 || range.isEmpty())
+    {
+        return 0;
+    }
+    return (height - 1) - static_cast<int>(std::round((value - range.getStart()) / range.getLength() * static_cast<double>(height)));
 }
 
 ANALYSE_FILE_END
--- Source/Zoom/AnlZoomTools.h
+++ Source/Zoom/AnlZoomTools.h
@@ -9,6 +9,10 @@ namespace Zoom
     namespace Tools
     {
         Range getScaledVisibleRange(Accessor const& zoomAcsr, Range const& newGlobalRange);
+        double getScaledValueFromWidth(Accessor const& zoomAcsr, juce::Component const& component, int x);
+        double getScaledValueFromHeight(Accessor const& zoomAcsr, juce::Component const& component, int y);
+        int getScaledXFromValue(Accessor const& zoomAcsr, juce::Component const& component, double value);
+        int getScaledYFromValue(Accessor const& zoomAcsr, juce::Component const& component, double value);
     }
 }
 
