869eb103dd20f39a3262c3f34f9f012387298f10
Pierre Guillot <guillotpierre6@gmail.com>
2021-04-08 18:07:58 +0200

    ColourButton: add class

--- Source/Misc/AnlColourSelector.cpp
+++ Source/Misc/AnlColourSelector.cpp
@@ -21,4 +21,78 @@ void ColourSelector::changeListenerCallback(juce::ChangeBroadcaster* source)
         onColourChanged(getCurrentColour());
     }
 }
+
+ColourButton::ColourButton(juce::Colour const& colour, juce::String const& title)
+: juce::ShapeButton("ColourButton", colour, colour.brighter(), colour.brighter())
+, mTitle(title)
+{
+    setClickingTogglesState(false);
+    setCurrentColour(colour, juce::NotificationType::dontSendNotification);
+}
+
+juce::Colour ColourButton::getCurrentColour() const
+{
+    return mColour;
+}
+
+void ColourButton::resized()
+{
+    juce::Path p;
+    p.addRoundedRectangle(getLocalBounds().toFloat(), 2.0f);
+    setShape(p, false, true, false);
+}
+
+void ColourButton::setTitle(juce::String const& title)
+{
+    mTitle = title;
+}
+
+void ColourButton::setCurrentColour(juce::Colour const& newColour, juce::NotificationType notificationType)
+{
+    if(newColour != mColour)
+    {
+        mColour = newColour;
+        setColours(mColour, mColour.brighter(), mColour.brighter());
+        repaint();
+        
+        if(notificationType == juce::NotificationType::sendNotificationAsync)
+        {
+            juce::WeakReference<juce::Component> target(this);
+            juce::MessageManager::callAsync([=, this]
+            {
+                if(target.get() != nullptr && onColourChanged != nullptr)
+                {
+                    onColourChanged(getCurrentColour());
+                }
+            });
+        }
+        else if(notificationType != juce::NotificationType::dontSendNotification)
+        {
+            if(onColourChanged != nullptr)
+            {
+                onColourChanged(getCurrentColour());
+            }
+        }
+    }
+}
+
+void ColourButton::clicked()
+{
+    ColourSelector colourSelector;
+    colourSelector.setSize(400, 300);
+    colourSelector.setCurrentColour(mColour, juce::NotificationType::dontSendNotification);
+    colourSelector.onColourChanged = [&](juce::Colour const& colour)
+    {
+        setCurrentColour(colour, juce::NotificationType::sendNotificationSync);
+    };
+    juce::DialogWindow::LaunchOptions options;
+    options.dialogTitle = juce::translate(mTitle);
+    options.content.setNonOwned(&colourSelector);
+    options.componentToCentreAround = this;
+    options.escapeKeyTriggersCloseButton = true;
+    options.useNativeTitleBar = true;
+    options.resizable = false;
+    options.runModal();
+}
+
 ANALYSE_FILE_END
--- Source/Misc/AnlColourSelector.h
+++ Source/Misc/AnlColourSelector.h
@@ -13,9 +13,34 @@ public:
     ~ColourSelector() override;
     
     std::function<void(juce::Colour const& colour)> onColourChanged = nullptr;
+    
 private:
     // juce::ChangeListener
     void changeListenerCallback(juce::ChangeBroadcaster* source) override;
+};
+
+class ColourButton
+: public juce::ShapeButton
+{
+public:
+    ColourButton(juce::Colour const& colour = juce::Colours::white, juce::String const& title = "");
+    ~ColourButton() override = default;
+    
+    void setTitle(juce::String const& title);
+    juce::Colour getCurrentColour() const;
+    void setCurrentColour(juce::Colour const& newColour, juce::NotificationType notificationType);
+    
+    std::function<void(juce::Colour const& colour)> onColourChanged = nullptr;
+    
+    // juce::Component
+    void resized() override;
+    
+private:
+    // juce::ShapeButton
+    void clicked() override;
+    
+    juce::String mTitle;
+    juce::Colour mColour;
 };
 
 ANALYSE_FILE_END
