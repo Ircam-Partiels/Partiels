be47fe497a7eecfd201778290b2d8e44df4f5708
Pierre Guillot <guillotpierre6@gmail.com>
2021-04-09 14:13:31 +0200

    Application::About: inherit from FloatingWindowContainer

--- Source/Application/AnlApplicationAbout.cpp
+++ Source/Application/AnlApplicationAbout.cpp
@@ -6,7 +6,8 @@
 ANALYSE_FILE_BEGIN
 
 Application::About::About()
-: mImage(juce::Drawable::createFromImageData(IconsData::icon_png, IconsData::icon_pngSize))
+: FloatingWindowContainer("About Partiels - v" + juce::String(ProjectInfo::versionString))
+, mImage(juce::Drawable::createFromImageData(IconsData::icon_png, IconsData::icon_pngSize))
 {
     setSize(400, 400);
 }
@@ -22,26 +23,6 @@ void Application::About::paint(juce::Graphics& g)
     g.setFont(16.0f);
     juce::String const text(MiscData::About_txt, MiscData::About_txtSize);
     g.drawFittedText(text, getLocalBounds().reduced(4), juce::Justification::left, 40);
-}
-
-void Application::About::show()
-{
-    if(mFloatingWindow.getContentComponent() == nullptr)
-    {
-        auto const& desktop = juce::Desktop::getInstance();
-        auto const mousePosition = desktop.getMainMouseSource().getScreenPosition().toInt();
-        auto const* display = desktop.getDisplays().getDisplayForPoint(mousePosition);
-        anlStrongAssert(display != nullptr);
-        if(display != nullptr)
-        {
-            auto const bounds = display->userArea.withSizeKeepingCentre(mFloatingWindow.getWidth(), mFloatingWindow.getHeight());
-            mFloatingWindow.setBounds(bounds);
-        }
-        mFloatingWindow.setContentNonOwned(this, true);
-    }
-
-    mFloatingWindow.setVisible(true);
-    mFloatingWindow.toFront(false);
 }
 
 ANALYSE_FILE_END
--- Source/Application/AnlApplicationAbout.h
+++ Source/Application/AnlApplicationAbout.h
@@ -7,20 +7,17 @@ ANALYSE_FILE_BEGIN
 namespace Application
 {
     class About
-    : public juce::Component
+    : public FloatingWindowContainer
     {
     public:
         About();
         ~About() override = default;
-        
-        void show();
         
         // juce::Component
         void paint(juce::Graphics& g) override;
         
     private:
         std::unique_ptr<juce::Drawable> mImage;
-        FloatingWindow mFloatingWindow {"About Partiels - v" + juce::String(ProjectInfo::versionString)};
     };
 }
 
