54ad46e90ce8a2d9f72153a59273d0c699e5a5a1
Pierre Guillot <guillotpierre6@gmail.com>
2021-04-15 15:27:04 +0200

    Transport::LoopBar: fix invalid paint

--- Source/Transport/AnlTransportLoopBar.cpp
+++ Source/Transport/AnlTransportLoopBar.cpp
@@ -72,13 +72,17 @@ void Transport::LoopBar::paint(juce::Graphics& g)
     g.setColour(findColour(ColourIds::thumbCoulourId));
     auto const x1 = Zoom::Tools::getScaledXFromValue(mZoomAccessor, *this, mLoopRange.getStart());
     auto const x2 = Zoom::Tools::getScaledXFromValue(mZoomAccessor, *this, mLoopRange.getEnd());
+    if(x1 <= x2)
+    {
+        return;
+    }
     if(mAccessor.getAttr<AttrType::looping>())
     {
         g.fillRoundedRectangle(static_cast<float>(x1), 0.0f, static_cast<float>(x2 - x1), static_cast<float>(getHeight()), 2.0f);
     }
     else
     {
-        g.drawRoundedRectangle(static_cast<float>(x1 + 1), 1.0f, static_cast<float>(x2 - x1 - 2), static_cast<float>(getHeight() - 2), 2.0f, 1.0f);
+        g.drawRoundedRectangle(static_cast<float>(x1 + 1), 1.0f, static_cast<float>(std::max(x2 - x1 - 2, 0)), static_cast<float>(std::max(getHeight() - 2, 0)), 2.0f, 1.0f);
     }
 }
 
