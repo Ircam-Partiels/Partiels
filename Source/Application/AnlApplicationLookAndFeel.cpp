8d4be5db8f60b761416a8cb827e47c7b570e7038
Pierre Guillot <guillotpierre6@gmail.com>
2021-04-19 11:10:52 +0200

    Application::LookAndFeel: format

--- Source/Application/AnlApplicationLookAndFeel.cpp
+++ Source/Application/AnlApplicationLookAndFeel.cpp
@@ -1,33 +1,33 @@
 #include "AnlApplicationLookAndFeel.h"
-#include "AnlApplicationFontManager.h"
-#include "../Zoom/AnlZoomRuler.h"
 #include "../Document/AnlDocumentSection.h"
+#include "../Zoom/AnlZoomRuler.h"
+#include "AnlApplicationFontManager.h"
 
 ANALYSE_FILE_BEGIN
 
 Application::LookAndFeel::LookAndFeel()
 {
     static FontManager fontManager;
-    
+
     auto const backgroundColour = juce::Colours::grey.darker();
     auto const rulerColour = juce::Colours::grey;
     auto const textColour = juce::Colours::white;
     auto const thumbColour = juce::Colours::white;
-    
+
     juce::Font::setDefaultMinimumHorizontalScaleFactor(1.0f);
-    
+
     setColour(ColourButton::ColourIds::borderOffColourId, rulerColour);
     setColour(ColourButton::ColourIds::borderOnColourId, rulerColour.brighter());
-    
+
     setColour(ColouredPanel::ColourIds::backgroundColourId, backgroundColour);
-    
+
     setColour(FloatingWindow::ColourIds::backgroundColourId, backgroundColour.darker());
 
     setColour(ConcertinaTable::ColourIds::headerBackgroundColourId, juce::Colours::transparentBlack);
     setColour(ConcertinaTable::ColourIds::headerBorderColourId, backgroundColour);
     setColour(ConcertinaTable::ColourIds::headerTitleColourId, textColour);
     setColour(ConcertinaTable::ColourIds::headerButtonColourId, textColour);
-    
+
     setColour(Decorator::ColourIds::backgroundColourId, backgroundColour.darker());
     setColour(Decorator::ColourIds::normalBorderColourId, backgroundColour);
     setColour(Decorator::ColourIds::highlightedBorderColourId, rulerColour.brighter());
@@ -35,25 +35,25 @@ Application::LookAndFeel::LookAndFeel()
     setColour(IconManager::ColourIds::normalColourId, rulerColour);
     setColour(IconManager::ColourIds::overColourId, rulerColour.brighter());
     setColour(IconManager::ColourIds::downColourId, rulerColour.brighter());
-    
+
     setColour(LoadingCircle::ColourIds::backgroundColourId, juce::Colours::transparentBlack);
     setColour(LoadingCircle::ColourIds::foregroundColourId, rulerColour);
-    
+
     setColour(ResizerBar::ColourIds::activeColourId, juce::Colours::transparentBlack);
     setColour(ResizerBar::ColourIds::inactiveColourId, juce::Colours::transparentBlack);
-    
+
     setColour(Zoom::Ruler::ColourIds::backgroundColourId, backgroundColour.darker());
     setColour(Zoom::Ruler::ColourIds::tickColourId, textColour);
     setColour(Zoom::Ruler::ColourIds::textColourId, textColour);
     setColour(Zoom::Ruler::ColourIds::anchorColourId, thumbColour);
     setColour(Zoom::Ruler::ColourIds::selectionColourId, thumbColour);
-    
+
     setColour(Transport::LoopBar::ColourIds::backgroundColourId, juce::Colours::transparentBlack);
     setColour(Transport::LoopBar::ColourIds::thumbCoulourId, thumbColour);
-    
+
     setColour(Transport::PlayheadBar::ColourIds::startPlayheadColourId, thumbColour);
     setColour(Transport::PlayheadBar::ColourIds::runningPlayheadColourId, rulerColour);
-    
+
     setColour(Track::Thumbnail::ColourIds::textColourId, textColour);
     setColour(Track::Thumbnail::ColourIds::titleBackgroundColourId, juce::Colours::transparentBlack);
     
@@ -64,37 +64,37 @@ Application::LookAndFeel::LookAndFeel()
     
     setColour(Group::Section::ColourIds::backgroundColourId, backgroundColour.darker());
     setColour(Group::Section::ColourIds::highlightedColourId, rulerColour.withAlpha(0.4f));
-    
+
     setColour(Document::Section::ColourIds::backgroundColourId, backgroundColour.darker());
-    
+
     auto& colourScheme = getCurrentColourScheme();
     colourScheme.setUIColour(ColourScheme::UIColour::windowBackground, backgroundColour.darker());
     colourScheme.setUIColour(ColourScheme::UIColour::widgetBackground, backgroundColour.darker());
-    
+
     // juce::ResizableWindow
     setColour(juce::ResizableWindow::ColourIds::backgroundColourId, backgroundColour.darker());
-    
+
     // juce::AlertWindow
     setColour(juce::AlertWindow::ColourIds::backgroundColourId, backgroundColour.darker());
-    
+
     // juce::TextButton
     setColour(juce::TextButton::ColourIds::buttonColourId, backgroundColour);
     setColour(juce::TextButton::ColourIds::buttonOnColourId, backgroundColour.brighter());
-    
+
     // juce::Slider
     setColour(juce::Slider::ColourIds::backgroundColourId, backgroundColour);
     setColour(juce::Slider::ColourIds::thumbColourId, thumbColour);
-    
+
     // juce::ComboBox
     setColour(juce::ComboBox::ColourIds::backgroundColourId, juce::Colours::transparentBlack);
     setColour(juce::ComboBox::ColourIds::outlineColourId, juce::Colours::transparentBlack);
     setColour(juce::ComboBox::ColourIds::focusedOutlineColourId, juce::Colours::transparentBlack);
     setColour(juce::ComboBox::ColourIds::arrowColourId, textColour);
-    
+
     // juce::ScrollBar
     setColour(juce::ScrollBar::ColourIds::backgroundColourId, backgroundColour.darker());
     setColour(juce::ScrollBar::ColourIds::thumbColourId, thumbColour);
-    
+
     // juce::ListBox
     setColour(juce::ListBox::ColourIds::backgroundColourId, backgroundColour);
     setColour(juce::ListBox::ColourIds::outlineColourId, backgroundColour.darker());
@@ -105,7 +105,7 @@ Application::LookAndFeel::LookAndFeel()
     setColour(juce::TableHeaderComponent::ColourIds::backgroundColourId, backgroundColour);
     setColour(juce::TableHeaderComponent::ColourIds::outlineColourId, backgroundColour.darker());
     setColour(juce::TableHeaderComponent::ColourIds::highlightColourId, backgroundColour.brighter());
-    
+
     // juce::TextEditor
     setColour(juce::TextEditor::ColourIds::backgroundColourId, juce::Colours::transparentBlack);
     setColour(juce::TextEditor::ColourIds::textColourId, textColour);
@@ -113,24 +113,24 @@ Application::LookAndFeel::LookAndFeel()
     setColour(juce::TextEditor::ColourIds::outlineColourId, juce::Colours::transparentBlack);
     setColour(juce::TextEditor::ColourIds::focusedOutlineColourId, juce::Colours::transparentBlack);
     setColour(juce::TextEditor::ColourIds::shadowColourId, juce::Colours::transparentBlack);
-    
+
     // juce::CaretComponent
     setColour(juce::CaretComponent::ColourIds::caretColourId, textColour);
-    
+
     // juce::PopupMenu
     setColour(juce::PopupMenu::ColourIds::backgroundColourId, backgroundColour.darker());
     setColour(juce::PopupMenu::ColourIds::textColourId, textColour);
     setColour(juce::PopupMenu::ColourIds::headerTextColourId, textColour);
     setColour(juce::PopupMenu::ColourIds::highlightedBackgroundColourId, backgroundColour);
     setColour(juce::PopupMenu::ColourIds::highlightedTextColourId, textColour);
-    
+
     // juce::ProgressBar
     setColour(juce::ProgressBar::ColourIds::backgroundColourId, backgroundColour);
     setColour(juce::ProgressBar::ColourIds::foregroundColourId, thumbColour);
-    
+
     // juce::HyperlinkButton
     setColour(juce::HyperlinkButton::ColourIds::textColourId, juce::Colour(0xFF16A4DB));
-    
+
     setDefaultSansSerifTypefaceName(fontManager.getDefaultSansSerifTypefaceName());
     setDefaultSansSerifTypeface(fontManager.getDefaultSansSerifTypeface());
 }
@@ -143,7 +143,7 @@ int Application::LookAndFeel::getHeaderHeight(ConcertinaTable const& panel) cons
 
 juce::Font Application::LookAndFeel::getHeaderFont(ConcertinaTable const& panel, int headerHeight) const
 {
-     juce::ignoreUnused(panel);
+    juce::ignoreUnused(panel);
     return juce::Font(static_cast<float>(headerHeight - 4));
 }
 
@@ -166,7 +166,7 @@ void Application::LookAndFeel::drawHeaderButton(juce::Graphics& g, ConcertinaTab
     auto const rotation = static_cast<float>(1.0 - sizeRatio) * juce::MathConstants<float>::pi / 2.0f;
     path.applyTransform(juce::AffineTransform::rotation(rotation, bounds.getCentre().getX(), bounds.getCentre().getY()));
     path.closeSubPath();
-    
+
     g.setColour(panel.findColour(ConcertinaTable::headerButtonColourId).brighter(contrast));
     g.fillPath(path);
 }
@@ -197,11 +197,11 @@ void Application::LookAndFeel::drawScrollbar(juce::Graphics& g, juce::ScrollBar&
 {
     juce::ignoreUnused(isMouseDown);
     g.fillAll(scrollbar.findColour(juce::ScrollBar::ColourIds::backgroundColourId));
-    
-    auto const thumbBounds = isScrollbarVertical ?  juce::Rectangle<int>{x, thumbStartPosition, width, thumbSize} : juce::Rectangle<int>{thumbStartPosition, y, thumbSize, height};
-    
+
+    auto const thumbBounds = isScrollbarVertical ? juce::Rectangle<int>{x, thumbStartPosition, width, thumbSize} : juce::Rectangle<int>{thumbStartPosition, y, thumbSize, height};
+
     auto const colour = scrollbar.findColour(juce::ScrollBar::ColourIds::thumbColourId);
-    g.setColour(isMouseOver ? colour.brighter (0.25f) :colour);
+    g.setColour(isMouseOver ? colour.brighter(0.25f) : colour);
     g.fillRoundedRectangle(thumbBounds.reduced(1).toFloat(), 4.0f);
 }
 
@@ -211,9 +211,9 @@ void Application::LookAndFeel::drawComboBox(juce::Graphics& g, int width, int he
     juce::Rectangle<int> bounds(0, 0, width, height);
     g.setColour(box.findColour(juce::ComboBox::ColourIds::backgroundColourId, true));
     g.fillRect(bounds);
-    g.setColour(box.findColour(box.hasKeyboardFocus(true) ? juce::ComboBox::ColourIds::focusedOutlineColourId :juce::ComboBox::ColourIds::outlineColourId, true));
+    g.setColour(box.findColour(box.hasKeyboardFocus(true) ? juce::ComboBox::ColourIds::focusedOutlineColourId : juce::ComboBox::ColourIds::outlineColourId, true));
     g.drawRect(bounds.reduced(1));
-    
+
     g.setColour(box.findColour(juce::ComboBox::ColourIds::arrowColourId).withAlpha(box.isEnabled() ? 1.0f : 0.2f));
     auto const arrawBounds = bounds.removeFromRight(height / 2).withSizeKeepingCentre(height / 2, height / 2).reduced(2).toFloat();
     juce::Path p;
@@ -224,13 +224,13 @@ void Application::LookAndFeel::drawComboBox(juce::Graphics& g, int width, int he
 void Application::LookAndFeel::drawAlertBox(juce::Graphics& g, juce::AlertWindow& alert, juce::Rectangle<int> const& textArea, juce::TextLayout& textLayout)
 {
     auto constexpr cornerSize = 4.0f;
-    
+
     g.setColour(alert.findColour(juce::AlertWindow::ColourIds::outlineColourId));
     g.drawRoundedRectangle(alert.getLocalBounds().toFloat(), cornerSize, 2.0f);
-    
+
     auto const bounds = alert.getLocalBounds().reduced(1);
     g.reduceClipRegion(bounds);
-    
+
     g.setColour(alert.findColour(juce::AlertWindow::ColourIds::backgroundColourId));
     g.fillRoundedRectangle(bounds.toFloat(), cornerSize);
 
@@ -249,9 +249,9 @@ void Application::LookAndFeel::drawAlertBox(juce::Graphics& g, juce::AlertWindow
         }
         return juce::Image();
     };
-    
+
     g.drawImage(getIcon(), {-48.0f, -20.0f, 180.0f, 180.0f});
-    
+
     g.setColour(alert.findColour(juce::AlertWindow::ColourIds::textColourId));
     textLayout.draw(g, textArea.withX(84).withTop(12).toFloat());
 }
@@ -270,9 +270,9 @@ void Application::LookAndFeel::drawTableHeaderColumn(juce::Graphics& g, juce::Ta
     }
     else if(isMouseOver)
     {
-        g.fillAll(header.findColour(juce::TableHeaderComponent::highlightColourId).withMultipliedAlpha (0.625f));
+        g.fillAll(header.findColour(juce::TableHeaderComponent::highlightColourId).withMultipliedAlpha(0.625f));
     }
-    
+
     g.setColour(header.findColour(juce::TableHeaderComponent::textColourId));
     auto area = juce::Rectangle<int>(width, height).reduced(4, 0);
     using ColumnPropertyFlags = juce::TableHeaderComponent::ColumnPropertyFlags;
@@ -333,45 +333,45 @@ void Application::LookAndFeel::drawLabel(juce::Graphics& g, juce::Label& label)
         auto const contrast = (isEditable && label.isMouseOverOrDragging()) ? 0.1f : 0.0f;
         auto const textColour = label.findColour(juce::Label::textColourId);
         g.setColour(isEditable && label.isMouseOver() ? textColour.contrasting(contrast) : textColour.withMultipliedAlpha(alpha));
-        
+
         auto const font = getLabelFont(label);
         g.setFont(font);
-        
+
         auto const caretWidth = label.isEditable() ? 3 : 0;
         auto const textArea = getLabelBorderSize(label).subtractedFrom(label.getLocalBounds()).withTrimmedRight(caretWidth);
-        
+
         g.drawFittedText(label.getText(), textArea, label.getJustificationType(), std::max(1, static_cast<int>(std::ceil(static_cast<float>(textArea.getHeight()) / font.getHeight()))), label.getMinimumHorizontalScale());
-        
+
         g.setColour(label.findColour(juce::Label::outlineColourId).withMultipliedAlpha(alpha));
     }
     else if(label.isEnabled())
     {
         g.setColour(label.findColour(juce::Label::outlineColourId));
     }
-    
+
     g.drawRect(label.getLocalBounds());
 }
 
 void Application::LookAndFeel::drawProgressBar(juce::Graphics& g, juce::ProgressBar& progressBar, int width, int height, double progress, juce::String const& textToShow)
 {
     juce::ignoreUnused(width, height);
-    
+
     auto const barBounds = progressBar.getLocalBounds().toFloat();
     auto const cornerSize = static_cast<float>(barBounds.getHeight()) * 0.5f;
-    
+
     auto const backgroundColour = progressBar.findColour(juce::ProgressBar::ColourIds::backgroundColourId);
     auto const foregroundColour = progressBar.findColour(juce::ProgressBar::ColourIds::foregroundColourId);
-    
+
     g.setColour(backgroundColour);
     g.fillRoundedRectangle(barBounds, cornerSize);
-    
+
     if(progress > 0.0)
     {
         g.setColour(foregroundColour);
         auto const progressWidth = barBounds.getWidth() * static_cast<float>(std::min(progress, 1.0));
         g.fillRoundedRectangle(barBounds.withWidth(progressWidth), cornerSize);
     }
-    
+
     if(!textToShow.isEmpty())
     {
         g.setColour(juce::Colour::contrasting(backgroundColour, foregroundColour));
@@ -398,17 +398,17 @@ void Application::LookAndFeel::drawTooltip(juce::Graphics& g, juce::String const
 {
     juce::Rectangle<float> const bounds(static_cast<float>(width), static_cast<float>(height));
     auto constexpr cornerSize = 5.0f;
-    
+
     g.setColour(findColour(juce::TooltipWindow::ColourIds::backgroundColourId));
     g.fillRoundedRectangle(bounds, cornerSize);
-    
+
     g.setColour(findColour(juce::TooltipWindow::ColourIds::outlineColourId));
     g.drawRoundedRectangle(bounds.reduced(0.5f, 0.5f), cornerSize, 1.0f);
-    
+
     juce::AttributedString attributedString;
     attributedString.setJustification(juce::Justification::centredLeft);
     attributedString.append(text, juce::Font(13.0f, juce::Font::bold), findColour(juce::TooltipWindow::ColourIds::textColourId));
-    
+
     juce::TextLayout tl;
     tl.createLayoutWithBalancedLineLengths(attributedString, 400.0f);
     tl.draw(g, bounds.reduced(4.0f, 4.0f));
@@ -418,41 +418,41 @@ void Application::LookAndFeel::drawButtonBackground(juce::Graphics& g, juce::But
 {
     auto constexpr cornerSize = 2.0f;
     auto const bounds = button.getLocalBounds().toFloat().reduced(0.5f, 0.5f);
-    
+
     auto baseColour = backgroundColour.withMultipliedSaturation(button.hasKeyboardFocus(true) ? 1.3f : 0.9f).withMultipliedAlpha(button.isEnabled() ? 1.0f : 0.5f);
-    
+
     if(shouldDrawButtonAsDown || shouldDrawButtonAsHighlighted)
     {
         baseColour = baseColour.contrasting(shouldDrawButtonAsDown ? 0.2f : 0.05f);
     }
-    
+
     g.setColour(baseColour);
-    
-    auto const flatOnLeft   = button.isConnectedOnLeft();
-    auto const flatOnRight  = button.isConnectedOnRight();
-    auto const flatOnTop    = button.isConnectedOnTop();
+
+    auto const flatOnLeft = button.isConnectedOnLeft();
+    auto const flatOnRight = button.isConnectedOnRight();
+    auto const flatOnTop = button.isConnectedOnTop();
     auto const flatOnBottom = button.isConnectedOnBottom();
-    
+
     if(flatOnLeft || flatOnRight || flatOnTop || flatOnBottom)
     {
         juce::Path path;
         path.addRoundedRectangle(bounds.getX(), bounds.getY(),
                                  bounds.getWidth(), bounds.getHeight(),
                                  cornerSize, cornerSize,
-                                 !(flatOnLeft  || flatOnTop),
+                                 !(flatOnLeft || flatOnTop),
                                  !(flatOnRight || flatOnTop),
-                                 !(flatOnLeft  || flatOnBottom),
+                                 !(flatOnLeft || flatOnBottom),
                                  !(flatOnRight || flatOnBottom));
-        
+
         g.fillPath(path);
-        
+
         g.setColour(button.findColour(juce::ComboBox::outlineColourId));
         g.strokePath(path, juce::PathStrokeType(1.0f));
     }
     else
     {
         g.fillRoundedRectangle(bounds, cornerSize);
-        
+
         g.setColour(button.findColour(juce::ComboBox::outlineColourId));
         g.drawRoundedRectangle(bounds, cornerSize, 1.0f);
     }
