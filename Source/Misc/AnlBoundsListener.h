73565125dc56b9c44852ae78c9a732d0eaf47183
Pierre Guillot <guillotpierre6@gmail.com>
2021-04-20 09:46:08 +0200

    BoundsListener: format

--- Source/Misc/AnlBoundsListener.h
+++ Source/Misc/AnlBoundsListener.h
@@ -8,22 +8,21 @@ class BoundsListener
 : private juce::ComponentListener
 {
 public:
-    
     BoundsListener() = default;
     ~BoundsListener() override;
-    
+
     void attachTo(juce::Component& component);
     void detachFrom(juce::Component& component);
-    
+
     std::function<void(juce::Component&)> onComponentMoved = nullptr;
     std::function<void(juce::Component&)> onComponentResized = nullptr;
+
 private:
-    
     // juce::ComponentListener
     void componentMovedOrResized(juce::Component& component, bool wasMoved, bool wasResized) override;
 
     std::set<juce::Component::SafePointer<juce::Component>> mContents;
-    
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(BoundsListener)
 };
 
