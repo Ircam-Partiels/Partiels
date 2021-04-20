36e00c412d3130c5f3a7fa89bd6c896e608bedd8
Pierre Guillot <guillotpierre6@gmail.com>
2021-04-20 09:47:18 +0200

    Chrono: format

--- Source/Misc/AnlChrono.h
+++ Source/Misc/AnlChrono.h
@@ -7,20 +7,22 @@ ANALYSE_FILE_BEGIN
 class Chrono
 {
 public:
-    
     Chrono(juce::String const& domain, juce::String const& message);
     ~Chrono() = default;
-    
+
 #ifdef DEBUG
     void start();
     void stop();
 #else
-    inline void start() const noexcept {}
-    inline void stop() const noexcept {}
+    inline void start() const noexcept
+    {
+    }
+    inline void stop() const noexcept
+    {
+    }
 #endif
-    
+
 private:
-    
     juce::String const mDomain;
     juce::String const mMessage;
 #ifdef DEBUG
