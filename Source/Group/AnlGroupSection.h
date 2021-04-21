bbf4a2346fe8781a4f3de84ed75624b336ff7248
Pierre Guillot <guillotpierre6@gmail.com>
2021-04-21 14:52:36 +0200

    Group::Section: format

--- Source/Group/AnlGroupSection.h
+++ Source/Group/AnlGroupSection.h
@@ -1,11 +1,11 @@
 #pragma once
 
+#include "../Transport/AnlTransportPlayheadBar.h"
 #include "../Zoom/AnlZoomRuler.h"
 #include "../Zoom/AnlZoomScrollBar.h"
-#include "../Transport/AnlTransportPlayheadBar.h"
-#include "AnlGroupThumbnail.h"
-#include "AnlGroupSnapshot.h"
 #include "AnlGroupPlot.h"
+#include "AnlGroupSnapshot.h"
+#include "AnlGroupThumbnail.h"
 
 ANALYSE_FILE_BEGIN
 
@@ -16,18 +16,20 @@ namespace Group
     , public juce::DragAndDropTarget
     {
     public:
+        // clang-format off
         enum ColourIds : int
         {
               backgroundColourId = 0x2040000
             , highlightedColourId
         };
-        
+        // clang-format on
+
         Section(Accessor& accessor, Transport::Accessor& transportAcsr, Zoom::Accessor& timeZoomAcsr);
         ~Section() override;
-        
+
         std::function<void(void)> onRemove = nullptr;
         std::function<void(juce::String const& identifier)> onTrackInserted = nullptr;
-        
+
         // juce::Component
         void resized() override;
         void paint(juce::Graphics& g) override;
@@ -44,29 +46,29 @@ namespace Group
         void itemDragMove(juce::DragAndDropTarget::SourceDetails const& dragSourceDetails) override;
         void itemDragExit(juce::DragAndDropTarget::SourceDetails const& dragSourceDetails) override;
         void itemDropped(juce::DragAndDropTarget::SourceDetails const& dragSourceDetails) override;
-        
+
         Accessor& mAccessor;
         Transport::Accessor& mTransportAccessor;
         Zoom::Accessor& mTimeZoomAccessor;
         Accessor::Listener mListener;
 
-        Thumbnail mThumbnail {mAccessor};
-        Decorator mThumbnailDecoration {mThumbnail, 1, 2.0f};
-        
-        Snapshot mSnapshot {mAccessor, mTransportAccessor, mTimeZoomAccessor};
-        Snapshot::Overlay mSnapshotOverlay {mSnapshot};
-        Decorator mSnapshotDecoration {mSnapshotOverlay, 1, 2.0f};
-        
-        Plot mPlot {mAccessor, mTransportAccessor, mTimeZoomAccessor};
-        Plot::Overlay mPlotOverlay {mPlot};
-        Decorator mPlotDecoration {mPlotOverlay, 1, 2.0f};
-        
-        Zoom::Ruler mRuler {mAccessor.getAcsr<AcsrType::zoom>(), Zoom::Ruler::Orientation::vertical};
-        Zoom::ScrollBar mScrollBar {mAccessor.getAcsr<AcsrType::zoom>(), Zoom::ScrollBar::Orientation::vertical, true};
+        Thumbnail mThumbnail{mAccessor};
+        Decorator mThumbnailDecoration{mThumbnail, 1, 2.0f};
 
-        ResizerBar mResizerBar {ResizerBar::Orientation::horizontal, true, {23, 2000}};
-        bool mIsItemDragged {false};
+        Snapshot mSnapshot{mAccessor, mTransportAccessor, mTimeZoomAccessor};
+        Snapshot::Overlay mSnapshotOverlay{mSnapshot};
+        Decorator mSnapshotDecoration{mSnapshotOverlay, 1, 2.0f};
+
+        Plot mPlot{mAccessor, mTransportAccessor, mTimeZoomAccessor};
+        Plot::Overlay mPlotOverlay{mPlot};
+        Decorator mPlotDecoration{mPlotOverlay, 1, 2.0f};
+
+        Zoom::Ruler mRuler{mAccessor.getAcsr<AcsrType::zoom>(), Zoom::Ruler::Orientation::vertical};
+        Zoom::ScrollBar mScrollBar{mAccessor.getAcsr<AcsrType::zoom>(), Zoom::ScrollBar::Orientation::vertical, true};
+
+        ResizerBar mResizerBar{ResizerBar::Orientation::horizontal, true, {23, 2000}};
+        bool mIsItemDragged{false};
     };
-}
+} // namespace Group
 
 ANALYSE_FILE_END
