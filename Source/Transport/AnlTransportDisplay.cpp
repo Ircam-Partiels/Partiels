57de8690048c8724cddec522966a6700005cca22
Pierre Guillot <guillotpierre6@gmail.com>
2021-04-21 14:47:18 +0200

    Transport::Display: format

--- Source/Transport/AnlTransportDisplay.cpp
+++ Source/Transport/AnlTransportDisplay.cpp
@@ -16,7 +16,7 @@ Transport::Display::Display(Accessor& accessor)
                 mPosition.setEnabled(!state);
                 lookAndFeelChanged();
             }
-                break;
+            break;
             case AttrType::startPlayhead:
             {
                 auto const state = acsr.getAttr<AttrType::playback>();
@@ -25,7 +25,7 @@ Transport::Display::Display(Accessor& accessor)
                     mPosition.setTime(acsr.getAttr<AttrType::startPlayhead>(), juce::NotificationType::dontSendNotification);
                 }
             }
-                break;
+            break;
             case AttrType::runningPlayhead:
             {
                 auto const state = acsr.getAttr<AttrType::playback>();
@@ -34,23 +34,23 @@ Transport::Display::Display(Accessor& accessor)
                     mPosition.setTime(acsr.getAttr<AttrType::runningPlayhead>(), juce::NotificationType::dontSendNotification);
                 }
             }
-                break;
+            break;
             case AttrType::looping:
             {
                 auto const state = acsr.getAttr<AttrType::looping>();
                 mLoopButton.setToggleState(state, juce::NotificationType::dontSendNotification);
             }
-                break;
+            break;
             case AttrType::loopRange:
             case AttrType::gain:
             {
                 auto const decibel = juce::Decibels::gainToDecibels(acsr.getAttr<AttrType::gain>(), -90.0);
                 mVolumeSlider.setValue(decibel, juce::NotificationType::dontSendNotification);
             }
-                break;
+            break;
         }
     };
-    
+
     mRewindButton.onClick = [&]()
     {
         auto const isPlaying = mAccessor.getAttr<Transport::AttrType::playback>();
@@ -64,19 +64,19 @@ Transport::Display::Display(Accessor& accessor)
             mAccessor.setAttr<Transport::AttrType::playback>(true, NotificationType::synchronous);
         }
     };
-    
+
     mPlaybackButton.setClickingTogglesState(true);
     mPlaybackButton.onClick = [&]()
     {
         mAccessor.setAttr<AttrType::playback>(mPlaybackButton.getToggleState(), NotificationType::synchronous);
     };
-    
+
     mLoopButton.setClickingTogglesState(true);
     mLoopButton.onClick = [&]()
     {
         mAccessor.setAttr<AttrType::looping>(mLoopButton.getToggleState(), NotificationType::synchronous);
     };
-    
+
     mVolumeSlider.setRange(-90.0, 12.0);
     mVolumeSlider.setDoubleClickReturnValue(true, 0.0);
     mVolumeSlider.onValueChange = [&]()
@@ -84,12 +84,11 @@ Transport::Display::Display(Accessor& accessor)
         auto const gain = std::min(juce::Decibels::decibelsToGain(mVolumeSlider.getValue(), -90.0), 12.0);
         mAccessor.setAttr<AttrType::gain>(gain, NotificationType::synchronous);
     };
-    
+
     mPosition.onTimeChanged = [&](double time)
     {
         mAccessor.setAttr<AttrType::startPlayhead>(time, NotificationType::synchronous);
     };
-    
 
     addAndMakeVisible(mRewindButton);
     mRewindButton.setWantsKeyboardFocus(false);
@@ -99,6 +98,7 @@ Transport::Display::Display(Accessor& accessor)
     mLoopButton.setWantsKeyboardFocus(false);
     addAndMakeVisible(mPosition);
     mPosition.setWantsKeyboardFocus(false);
+
     addAndMakeVisible(mVolumeSlider);
     mAccessor.addListener(mListener, NotificationType::synchronous);
 }
@@ -116,7 +116,7 @@ void Transport::Display::resized()
     mRewindButton.setBounds(bounds.removeFromLeft(buttonSize).reduced(4));
     mPlaybackButton.setBounds(bounds.removeFromLeft(buttonSize).reduced(4));
     mLoopButton.setBounds(bounds.removeFromLeft(buttonSize).reduced(4));
-    
+
     mVolumeSlider.setBounds(bounds.removeFromBottom(buttonSize / 3));
     mPosition.setBounds(bounds);
     mPosition.setFont(juce::Font(static_cast<float>(bounds.getHeight()) * 0.8f));
