aa1027013fcef1682fd5fa05dbd07dbd1cfb96d6
Pierre Guillot <guillotpierre6@gmail.com>
2021-04-22 15:12:01 +0200

    Application::Interface: format

--- Source/Application/AnlApplicationInterface.h
+++ Source/Application/AnlApplicationInterface.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include "AnlApplicationCommandTarget.h"
-#include "../Document/AnlDocumentModel.h"
 #include "../Document/AnlDocumentFileInfoPanel.h"
+#include "../Document/AnlDocumentModel.h"
 #include "../Document/AnlDocumentSection.h"
 #include "../Transport/AnlTransportDisplay.h"
+#include "AnlApplicationCommandTarget.h"
 
 ANALYSE_FILE_BEGIN
 
@@ -18,31 +18,31 @@ namespace Application
     public:
         Interface();
         ~Interface() override;
-        
+
         void moveKeyboardFocusTo(juce::String const& identifier);
 
         // juce::Component
         void resized() override;
-        
+
         // juce::FileDragAndDropTarget
         bool isInterestedInFileDrag(juce::StringArray const& files) override;
         void fileDragEnter(juce::StringArray const& files, int x, int y) override;
         void fileDragExit(juce::StringArray const& files) override;
         void filesDropped(juce::StringArray const& files, int x, int y) override;
+
     private:
         Document::Accessor::Listener mDocumentListener;
-        
+
         // Header
         Transport::Display mTransportDisplay;
-        juce::TextButton mLoad {juce::translate("Load File")};
-        
+        juce::TextButton mLoad{juce::translate("Load File")};
+
         // Main
         Document::Section mDocumentSection;
-        
+
         //Footer
         Tooltip::Display mToolTipDisplay;
     };
-}
+} // namespace Application
 
 ANALYSE_FILE_END
-
