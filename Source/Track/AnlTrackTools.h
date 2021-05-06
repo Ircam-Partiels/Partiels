7db312efe1879c7fce09907b01210ccb2856bf89
Pierre Guillot <guillotpierre6@gmail.com>
2021-05-06 13:49:32 +0200

    Track::Track::Tools: forrmat

--- Source/Track/AnlTrackTools.h
+++ Source/Track/AnlTrackTools.h
@@ -8,15 +8,17 @@ namespace Track
 {
     namespace Tools
     {
+        // clang-format off
         enum class DisplayType
         {
               markers
             , segments
             , grid
         };
-        
+        // clang-format on
+
         DisplayType getDisplayType(Accessor const& acsr);
-        
+
         float valueToPixel(float value, juce::Range<double> const& valueRange, juce::Rectangle<float> const& bounds);
         float secondsToPixel(double seconds, juce::Range<double> const& timeRange, juce::Rectangle<float> const& bounds);
         double pixelToSeconds(float position, juce::Range<double> const& timeRange, juce::Rectangle<float> const& bounds);
@@ -32,12 +34,12 @@ namespace Track
         juce::String getGridText(std::vector<Plugin::Result> const& results, Plugin::Output const& output, Zoom::Range const& globalRange, double time, size_t bin);
         juce::String getResultText(Accessor const& acsr, Zoom::Range const& globalRange, double time, size_t bin, double timeEpsilon);
         juce::String getProcessingTooltip(Accessor const& acsr);
-    
+
         std::optional<Zoom::Range> getValueRange(Plugin::Description const& description);
         std::optional<Zoom::Range> getValueRange(std::shared_ptr<const std::vector<Plugin::Result>> const& results);
         std::optional<Zoom::Range> getBinRange(Plugin::Description const& description);
         std::optional<Zoom::Range> getBinRange(std::shared_ptr<const std::vector<Plugin::Result>> const& results);
-    }
-}
+    } // namespace Tools
+} // namespace Track
 
 ANALYSE_FILE_END
