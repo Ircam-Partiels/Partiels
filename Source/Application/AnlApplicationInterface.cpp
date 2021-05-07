31d6666525f406ce3c956ec111bc4994289dba22
Pierre Guillot <guillotpierre6@gmail.com>
2021-05-07 16:35:53 +0200

    Application::Interface: improve startup

--- Source/Application/AnlApplicationInterface.cpp
+++ Source/Application/AnlApplicationInterface.cpp
@@ -3,40 +3,32 @@
 
 ANALYSE_FILE_BEGIN
 
-Application::Interface::Interface()
-: mTransportDisplay(Instance::get().getDocumentAccessor().getAcsr<Document::AcsrType::transport>())
-, mDocumentSection(Instance::get().getDocumentDirector())
+Application::Interface::Loader::Loader()
 {
-    addAndMakeVisible(mTransportDisplay);
-    addAndMakeVisible(mDocumentSection);
-    addAndMakeVisible(mToolTipDisplay);
-
-    mLoad.onClick = []()
+    using CommandIDs = CommandTarget::CommandIDs;
+    auto& commandManager = Instance::get().getApplicationCommandManager();
+    mLoadFileButton.onClick = [&]()
     {
-        using CommandIDs = CommandTarget::CommandIDs;
-        Instance::get().getApplicationCommandManager().invokeDirectly(CommandIDs::DocumentOpen, true);
+        commandManager.invokeDirectly(CommandIDs::DocumentOpen, true);
     };
-
-    mDocumentListener.onAttrChanged = [&](Document::Accessor const& acsr, Document::AttrType attribute)
+    mAddTrackButton.onClick = [&]()
     {
+        commandManager.invokeDirectly(CommandIDs::EditNewTrack, true);
+    };
+    mLoadTemplateButton.onClick = [&]()
+    {
+        commandManager.invokeDirectly(CommandIDs::DocumentOpenTemplate, true);
+    };
+    mLoadFileLabel.setJustificationType(juce::Justification::centred);
+
+    mDocumentListener.onAttrChanged = [this](Document::Accessor const& acsr, Document::AttrType attribute)
+    {
+        juce::ignoreUnused(acsr);
         switch(attribute)
         {
             case Document::AttrType::file:
             {
-                auto const file = acsr.getAttr<Document::AttrType::file>();
-                auto const& audioFormatManager = Instance::get().getAudioFormatManager();
-                auto const isDocumentEnable = file.existsAsFile() && audioFormatManager.getWildcardForAllFormats().contains(file.getFileExtension());
-
-                mTransportDisplay.setEnabled(isDocumentEnable);
-                mDocumentSection.setEnabled(isDocumentEnable);
-                if(!isDocumentEnable)
-                {
-                    addAndMakeVisible(mLoad);
-                }
-                else
-                {
-                    removeChildComponent(&mLoad);
-                }
+                updateState();
             }
             break;
             case Document::AttrType::layout:
@@ -44,27 +36,87 @@ Application::Interface::Interface()
         }
     };
 
+    mDocumentListener.onAccessorErased = mDocumentListener.onAccessorInserted = [this](Document::Accessor const& acsr, Document::AcsrType type, size_t index)
+    {
+        juce::ignoreUnused(acsr, type, index);
+        updateState();
+    };
+
     auto& documentAccessor = Instance::get().getDocumentAccessor();
     documentAccessor.addListener(mDocumentListener, NotificationType::synchronous);
+    commandManager.addListener(this);
 }
 
-Application::Interface::~Interface()
+Application::Interface::Loader::~Loader()
 {
+    Instance::get().getApplicationCommandManager().removeListener(this);
     auto& documentAccessor = Instance::get().getDocumentAccessor();
     documentAccessor.removeListener(mDocumentListener);
 }
 
-void Application::Interface::resized()
+void Application::Interface::Loader::updateState()
 {
-    auto bounds = getLocalBounds();
-    mTransportDisplay.setBounds(bounds.removeFromTop(40).withSizeKeepingCentre(284, 40));
-    mToolTipDisplay.setBounds(bounds.removeFromBottom(24));
-    mLoad.setBounds(bounds.withSizeKeepingCentre(200, 32));
-    mDocumentSection.setBounds(bounds);
+    auto& documentAccessor = Instance::get().getDocumentAccessor();
+    auto const documentHasFile = documentAccessor.getAttr<Document::AttrType::file>().existsAsFile();
+    auto const documentHasTrackOrGroup = documentAccessor.getNumAcsr<Document::AcsrType::tracks>() > 0_z || documentAccessor.getNumAcsr<Document::AcsrType::groups>() > 0_z;
+
+    removeChildComponent(&mLoadFileButton);
+    removeChildComponent(&mLoadFileLabel);
+    removeChildComponent(&mAddTrackButton);
+    removeChildComponent(&mLoadTemplateButton);
+    setVisible(!documentHasFile || !documentHasTrackOrGroup);
+    if(!documentHasFile)
+    {
+        addAndMakeVisible(mLoadFileButton);
+        addAndMakeVisible(mLoadFileLabel);
+    }
+    else if(!documentHasTrackOrGroup)
+    {
+        addAndMakeVisible(mAddTrackButton);
+        addAndMakeVisible(mLoadTemplateButton);
+    }
 }
 
-bool Application::Interface::isInterestedInFileDrag(juce::StringArray const& files)
+void Application::Interface::Loader::resized()
 {
+    auto const bounds = getLocalBounds();
+    {
+        auto innerBounds = bounds.withSizeKeepingCentre(200, 100);
+        mLoadFileButton.setBounds(innerBounds.removeFromTop(32));
+        mLoadFileLabel.setBounds(innerBounds);
+    }
+    {
+        auto innerBounds = bounds.withSizeKeepingCentre(402, 32);
+        mAddTrackButton.setBounds(innerBounds.removeFromLeft(200));
+        mLoadTemplateButton.setBounds(innerBounds.withTrimmedLeft(2));
+    }
+}
+
+void Application::Interface::Loader::paint(juce::Graphics& g)
+{
+    g.fillAll(mIsDragging ? findColour(juce::TextButton::ColourIds::buttonColourId) : findColour(juce::ResizableWindow::ColourIds::backgroundColourId));
+}
+
+void Application::Interface::Loader::applicationCommandInvoked(juce::ApplicationCommandTarget::InvocationInfo const& info)
+{
+    juce::ignoreUnused(info);
+}
+
+void Application::Interface::Loader::applicationCommandListChanged()
+{
+    using CommandIDs = CommandTarget::CommandIDs;
+    auto& commandManager = Instance::get().getApplicationCommandManager();
+    mLoadFileButton.setTooltip(commandManager.getDescriptionOfCommand(CommandIDs::DocumentOpen));
+    mAddTrackButton.setTooltip(commandManager.getDescriptionOfCommand(CommandIDs::EditNewTrack));
+    mLoadTemplateButton.setTooltip(commandManager.getDescriptionOfCommand(CommandIDs::DocumentOpenTemplate));
+}
+
+bool Application::Interface::Loader::isInterestedInFileDrag(juce::StringArray const& files)
+{
+    if(!mLoadFileButton.isVisible())
+    {
+        return false;
+    }
     auto const audioFormatWildcard = Instance::get().getAudioFormatManager().getWildcardForAllFormats() + ";" + Instance::getFileWildCard();
     for(auto const& fileName : files)
     {
@@ -76,22 +128,27 @@ bool Application::Interface::isInterestedInFileDrag(juce::StringArray const& fil
     return false;
 }
 
-void Application::Interface::fileDragEnter(juce::StringArray const& files, int x, int y)
+void Application::Interface::Loader::fileDragEnter(juce::StringArray const& files, int x, int y)
 {
+    mIsDragging = true;
     juce::ignoreUnused(files, x, y);
-    mLoad.setState(juce::Button::ButtonState::buttonOver);
+    mLoadFileButton.setState(juce::Button::ButtonState::buttonOver);
+    repaint();
 }
 
-void Application::Interface::fileDragExit(juce::StringArray const& files)
+void Application::Interface::Loader::fileDragExit(juce::StringArray const& files)
 {
+    mIsDragging = false;
     juce::ignoreUnused(files);
-    mLoad.setState(juce::Button::ButtonState::buttonNormal);
+    mLoadFileButton.setState(juce::Button::ButtonState::buttonNormal);
+    repaint();
 }
 
-void Application::Interface::filesDropped(juce::StringArray const& files, int x, int y)
+void Application::Interface::Loader::filesDropped(juce::StringArray const& files, int x, int y)
 {
+    mIsDragging = false;
     juce::ignoreUnused(x, y);
-    mLoad.setState(juce::Button::ButtonState::buttonNormal);
+    mLoadFileButton.setState(juce::Button::ButtonState::buttonNormal);
     auto const audioFormatWildcard = Instance::get().getAudioFormatManager().getWildcardForAllFormats() + ";" + Instance::getFileWildCard();
     auto getFile = [&]()
     {
@@ -110,11 +167,49 @@ void Application::Interface::filesDropped(juce::StringArray const& files, int x,
         return;
     }
     Instance::get().openFile(file);
+    repaint();
+}
+
+Application::Interface::Interface()
+: mTransportDisplay(Instance::get().getDocumentAccessor().getAcsr<Document::AcsrType::transport>())
+, mDocumentSection(Instance::get().getDocumentDirector())
+{
+    addAndMakeVisible(mTransportDisplay);
+    addAndMakeVisible(mDocumentSection);
+    addAndMakeVisible(mLoaderDecorator);
+    addAndMakeVisible(mToolTipDisplay);
+    mLoader.addComponentListener(this);
+}
+
+Application::Interface::~Interface()
+{
+    mLoader.removeComponentListener(this);
+}
+
+void Application::Interface::resized()
+{
+    auto bounds = getLocalBounds();
+    mTransportDisplay.setBounds(bounds.removeFromTop(40).withSizeKeepingCentre(284, 40));
+    mToolTipDisplay.setBounds(bounds.removeFromBottom(24));
+    mDocumentSection.setBounds(bounds);
+    mLoaderDecorator.setBounds(bounds);
 }
 
 void Application::Interface::moveKeyboardFocusTo(juce::String const& identifier)
 {
     mDocumentSection.moveKeyboardFocusTo(identifier);
+}
+
+void Application::Interface::componentVisibilityChanged(juce::Component& component)
+{
+    anlStrongAssert(&component == &mLoader);
+    if(&component != &mLoader)
+    {
+        return;
+    }
+    mLoaderDecorator.setVisible(mLoader.isVisible());
+    mTransportDisplay.setVisible(!mLoader.isVisible());
+    mDocumentSection.setVisible(!mLoader.isVisible());
 }
 
 ANALYSE_FILE_END
--- Source/Application/AnlApplicationInterface.h
+++ Source/Application/AnlApplicationInterface.h
@@ -12,8 +12,8 @@ namespace Application
 {
     class Interface
     : public juce::Component
-    , public juce::FileDragAndDropTarget
     , public CommandTarget
+    , private juce::ComponentListener
     {
     public:
         Interface();
@@ -24,23 +24,48 @@ namespace Application
         // juce::Component
         void resized() override;
 
-        // juce::FileDragAndDropTarget
-        bool isInterestedInFileDrag(juce::StringArray const& files) override;
-        void fileDragEnter(juce::StringArray const& files, int x, int y) override;
-        void fileDragExit(juce::StringArray const& files) override;
-        void filesDropped(juce::StringArray const& files, int x, int y) override;
-
     private:
-        Document::Accessor::Listener mDocumentListener;
+        // juce::ComponentListener
+        void componentVisibilityChanged(juce::Component& component) override;
 
-        // Header
+        class Loader
+        : public juce::Component
+        , public juce::FileDragAndDropTarget
+        , private juce::ApplicationCommandManagerListener
+        {
+        public:
+            Loader();
+            ~Loader() override;
+
+            void updateState();
+
+            // juce::Component
+            void resized() override;
+            void paint(juce::Graphics& g) override;
+
+            // juce::FileDragAndDropTarget
+            bool isInterestedInFileDrag(juce::StringArray const& files) override;
+            void fileDragEnter(juce::StringArray const& files, int x, int y) override;
+            void fileDragExit(juce::StringArray const& files) override;
+            void filesDropped(juce::StringArray const& files, int x, int y) override;
+
+        private:
+            // juce::ApplicationCommandManagerListener
+            void applicationCommandInvoked(juce::ApplicationCommandTarget::InvocationInfo const& info) override;
+            void applicationCommandListChanged() override;
+
+            Document::Accessor::Listener mDocumentListener;
+            juce::TextButton mLoadFileButton{juce::translate("Load")};
+            juce::Label mLoadFileLabel{"", juce::translate("Or\nDrag & Drop\n(Audio/Document)")};
+            juce::TextButton mAddTrackButton{juce::translate("Add Track")};
+            juce::TextButton mLoadTemplateButton{juce::translate("Load Template")};
+            bool mIsDragging{false};
+        };
+
         Transport::Display mTransportDisplay;
-        juce::TextButton mLoad{juce::translate("Load File")};
-
-        // Main
         Document::Section mDocumentSection;
-
-        //Footer
+        Loader mLoader;
+        Decorator mLoaderDecorator{mLoader, 1, 2.0f};
         Tooltip::Display mToolTipDisplay;
     };
 } // namespace Application
