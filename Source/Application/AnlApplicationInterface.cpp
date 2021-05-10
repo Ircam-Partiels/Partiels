92a6aa6c8d782905fb968f9a90daac18ee19a39f
Pierre Guillot <guillotpierre6@gmail.com>
2021-05-10 12:09:29 +0200

    Application::Interface: improve loader

--- Source/Application/AnlApplicationInterface.cpp
+++ Source/Application/AnlApplicationInterface.cpp
@@ -3,6 +3,99 @@
 
 ANALYSE_FILE_BEGIN
 
+Application::Interface::Loader::FileTable::FileTable()
+{
+    addAndMakeVisible(mListBox);
+
+    mListener.onAttrChanged = [this](Accessor const& acsr, AttrType attribute)
+    {
+        juce::ignoreUnused(acsr);
+        switch(attribute)
+        {
+            case AttrType::windowState:
+                break;
+            case AttrType::recentlyOpenedFilesList:
+            {
+                mListBox.updateContent();
+                mListBox.repaint();
+            }
+            break;
+            case AttrType::currentDocumentFile:
+                break;
+            case AttrType::colourMode:
+                break;
+        }
+    };
+
+    auto& accessor = Instance::get().getApplicationAccessor();
+    accessor.addListener(mListener, NotificationType::synchronous);
+}
+
+Application::Interface::Loader::FileTable::~FileTable()
+{
+    auto& accessor = Instance::get().getApplicationAccessor();
+    accessor.removeListener(mListener);
+}
+
+void Application::Interface::Loader::FileTable::resized()
+{
+    mListBox.setBounds(getLocalBounds());
+}
+
+int Application::Interface::Loader::FileTable::getNumRows()
+{
+    auto const& accessor = Instance::get().getApplicationAccessor();
+    return static_cast<int>(accessor.getAttr<AttrType::recentlyOpenedFilesList>().size());
+}
+
+void Application::Interface::Loader::FileTable::paintListBoxItem(int rowNumber, juce::Graphics& g, int width, int height, bool rowIsSelected)
+{
+    const auto defaultColour = mListBox.findColour(juce::ListBox::backgroundColourId);
+    const auto selectedColour = defaultColour.interpolatedWith(mListBox.findColour(juce::ListBox::textColourId), 0.5f);
+    g.fillAll(rowIsSelected ? selectedColour : defaultColour);
+
+    auto const& accessor = Instance::get().getApplicationAccessor();
+    auto const& files = accessor.getAttr<AttrType::recentlyOpenedFilesList>();
+    if(rowNumber >= static_cast<int>(files.size()))
+    {
+        return;
+    }
+
+    auto const index = static_cast<size_t>(rowNumber);
+
+    auto const fileName = files[index].getFileNameWithoutExtension();
+    auto const hasDuplicate = std::count_if(files.cbegin(), files.cend(), [&](auto const file)
+                                            {
+                                                return fileName == file.getFileNameWithoutExtension();
+                                            }) > 1l;
+    auto const text = fileName + (hasDuplicate ? " (" + files[index].getParentDirectory().getFileName() + ")" : "");
+
+    g.setColour(mListBox.findColour(juce::ListBox::textColourId));
+    g.setFont(juce::Font(static_cast<float>(height) * 0.7f, juce::Font::bold));
+    g.drawFittedText(text, 4, 0, width - 6, height, juce::Justification::centredLeft, 1, 1.f);
+}
+
+void Application::Interface::Loader::FileTable::returnKeyPressed(int lastRowSelected)
+{
+    auto const& accessor = Instance::get().getApplicationAccessor();
+    auto const& files = accessor.getAttr<AttrType::recentlyOpenedFilesList>();
+    if(lastRowSelected >= static_cast<int>(files.size()))
+    {
+        return;
+    }
+    auto const index = static_cast<size_t>(lastRowSelected);
+    if(onFileSelected != nullptr)
+    {
+        onFileSelected(files[index]);
+    }
+}
+
+void Application::Interface::Loader::FileTable::listBoxItemDoubleClicked(int row, juce::MouseEvent const& event)
+{
+    juce::ignoreUnused(event);
+    returnKeyPressed(row);
+}
+
 Application::Interface::Loader::Loader()
 {
     using CommandIDs = CommandTarget::CommandIDs;
@@ -19,7 +112,50 @@ Application::Interface::Loader::Loader()
     {
         commandManager.invokeDirectly(CommandIDs::EditLoadTemplate, true);
     };
-    mLoadFileLabel.setJustificationType(juce::Justification::centred);
+    mLoadFileInfo.setText(juce::translate("or\nDrag & Drop\n(Document/Audio)"), juce::NotificationType::dontSendNotification);
+    mLoadFileInfo.setJustificationType(juce::Justification::centredTop);
+    
+    mLoadFileWildcard.setText(juce::translate("Document: DOCWILDCARD\nAudio: AUDIOWILDCARD").replace("DOCWILDCARD", Instance::getFileWildCard()).replace("AUDIOWILDCARD", "*.aac,*.aiff,*.aif,*.flac,*.m4a,*.mp3,*.ogg,*.wav,*.wma"), juce::NotificationType::dontSendNotification);
+    mLoadFileWildcard.setJustificationType(juce::Justification::bottomLeft);
+    
+    mAddTrackInfo.setText(juce::translate("Insert an analysis plugin as a new track"), juce::NotificationType::dontSendNotification);
+    mAddTrackInfo.setJustificationType(juce::Justification::centredTop);
+    
+    mLoadTemplateInfo.setText(juce::translate("Use an existing document as template"), juce::NotificationType::dontSendNotification);
+    mLoadTemplateInfo.setJustificationType(juce::Justification::centredTop);
+
+    addAndMakeVisible(mSeparatorVertical);
+    addAndMakeVisible(mSeparatorHorizontal);
+    addAndMakeVisible(mSelectRecentDocument);
+    addAndMakeVisible(mFileTable);
+    
+    mFileTable.onFileSelected = [](juce::File const& file)
+    {
+        auto& documentAcsr = Instance::get().getDocumentAccessor();
+        auto const documentHasFile = documentAcsr.getAttr<Document::AttrType::file>().existsAsFile();
+        if(!documentHasFile)
+        {
+            Instance::get().openFile(file);
+        }
+        else
+        {
+            Document::Accessor copyAcsr;
+            juce::UndoManager copyUndoManager;
+            Document::Director copyDirector{copyAcsr, Instance::get().getAudioFormatManager(), copyUndoManager};
+            Document::FileBased copyFileBased{copyAcsr, copyDirector, Instance::getFileExtension(), Instance::getFileWildCard(), "Open a document", "Save the document"};
+            if(copyFileBased.loadFrom(file, true).failed())
+            {
+                return;
+            }
+        
+            copyAcsr.setAttr<Document::AttrType::file>(documentAcsr.getAttr<Document::AttrType::file>(), NotificationType::synchronous);
+            
+            auto& documentDir = Instance::get().getDocumentDirector();
+            documentDir.startAction();
+            documentAcsr.copyFrom(copyAcsr, NotificationType::synchronous);
+            documentDir.endAction("Load template", ActionState::apply);
+        }
+    };
 
     mDocumentListener.onAttrChanged = [this](Document::Accessor const& acsr, Document::AttrType attribute)
     {
@@ -61,40 +197,76 @@ void Application::Interface::Loader::updateState()
     auto const documentHasTrackOrGroup = documentAccessor.getNumAcsr<Document::AcsrType::tracks>() > 0_z || documentAccessor.getNumAcsr<Document::AcsrType::groups>() > 0_z;
 
     removeChildComponent(&mLoadFileButton);
-    removeChildComponent(&mLoadFileLabel);
+    removeChildComponent(&mLoadFileInfo);
+    removeChildComponent(&mLoadFileWildcard);
     removeChildComponent(&mAddTrackButton);
+    removeChildComponent(&mAddTrackInfo);
     removeChildComponent(&mLoadTemplateButton);
+    removeChildComponent(&mLoadTemplateInfo);
     setVisible(!documentHasFile || !documentHasTrackOrGroup);
     if(!documentHasFile)
     {
         addAndMakeVisible(mLoadFileButton);
-        addAndMakeVisible(mLoadFileLabel);
+        addAndMakeVisible(mLoadFileInfo);
+        addAndMakeVisible(mLoadFileWildcard);
     }
     else if(!documentHasTrackOrGroup)
     {
         addAndMakeVisible(mAddTrackButton);
+        addAndMakeVisible(mAddTrackInfo);
         addAndMakeVisible(mLoadTemplateButton);
+        addAndMakeVisible(mLoadTemplateInfo);
     }
 }
 
 void Application::Interface::Loader::resized()
 {
-    auto const bounds = getLocalBounds();
+    auto bounds = getLocalBounds();
     {
-        auto innerBounds = bounds.withSizeKeepingCentre(200, 100);
-        mLoadFileButton.setBounds(innerBounds.removeFromTop(32));
-        mLoadFileLabel.setBounds(innerBounds);
+        auto rightBounds = bounds.removeFromRight(220);
+        mSelectRecentDocument.setBounds(rightBounds.removeFromTop(32));
+        mSeparatorHorizontal.setBounds(rightBounds.removeFromTop(1));
+        mFileTable.setBounds(rightBounds);
+        mSeparatorVertical.setBounds(bounds.removeFromRight(1));
     }
     {
-        auto innerBounds = bounds.withSizeKeepingCentre(402, 32);
-        mAddTrackButton.setBounds(innerBounds.removeFromLeft(200));
-        mLoadTemplateButton.setBounds(innerBounds.withTrimmedLeft(2));
+        auto centerBounds = bounds.withSizeKeepingCentre(160, 82);
+        mLoadFileButton.setBounds(centerBounds.removeFromTop(32));
+        mLoadFileInfo.setBounds(centerBounds);
+        auto bottomBounds = bounds;
+        mLoadFileWildcard.setBounds(bottomBounds.removeFromBottom(64));
     }
+    {
+        auto centerBounds = bounds.withSizeKeepingCentre(402, 82);
+        {
+            auto centerLeftBounds = centerBounds.removeFromLeft(200);
+            mAddTrackButton.setBounds(centerLeftBounds.removeFromTop(32));
+            mAddTrackInfo.setBounds(centerLeftBounds);
+        }
+        {
+            auto centerRightBounds = centerBounds.withTrimmedLeft(2);
+            mLoadTemplateButton.setBounds(centerRightBounds.removeFromTop(32));
+            mLoadTemplateInfo.setBounds(centerRightBounds.removeFromTop(32));
+        }
+    }
+}
+
+void Application::Interface::Loader::lookAndFeelChanged()
+{
+    auto const colour = getLookAndFeel().findColour(juce::TableListBox::ColourIds::outlineColourId);
+    mSeparatorVertical.setColour(ColouredPanel::backgroundColourId, colour);
+    mSeparatorHorizontal.setColour(ColouredPanel::backgroundColourId, colour);
+}
+
+void Application::Interface::Loader::visibilityChanged()
+{
+    lookAndFeelChanged();
 }
 
 void Application::Interface::Loader::paint(juce::Graphics& g)
 {
-    g.fillAll(mIsDragging ? findColour(juce::TextButton::ColourIds::buttonColourId) : findColour(juce::ResizableWindow::ColourIds::backgroundColourId));
+    g.setColour(mIsDragging ? findColour(juce::TextButton::ColourIds::buttonColourId) : findColour(juce::ResizableWindow::ColourIds::backgroundColourId));
+    g.fillRoundedRectangle(getLocalBounds().withTrimmedRight(222).toFloat(), 2.0f);
 }
 
 void Application::Interface::Loader::applicationCommandInvoked(juce::ApplicationCommandTarget::InvocationInfo const& info)
@@ -192,7 +364,7 @@ void Application::Interface::resized()
     mTransportDisplay.setBounds(bounds.removeFromTop(40).withSizeKeepingCentre(284, 40));
     mToolTipDisplay.setBounds(bounds.removeFromBottom(24));
     mDocumentSection.setBounds(bounds);
-    mLoaderDecorator.setBounds(bounds);
+    mLoaderDecorator.setBounds(bounds.withSizeKeepingCentre(800, 600));
 }
 
 void Application::Interface::moveKeyboardFocusTo(juce::String const& identifier)
@@ -208,8 +380,8 @@ void Application::Interface::componentVisibilityChanged(juce::Component& compone
         return;
     }
     mLoaderDecorator.setVisible(mLoader.isVisible());
-    mTransportDisplay.setVisible(!mLoader.isVisible());
-    mDocumentSection.setVisible(!mLoader.isVisible());
+    mTransportDisplay.setEnabled(!mLoader.isVisible());
+    mDocumentSection.setEnabled(!mLoader.isVisible());
 }
 
 ANALYSE_FILE_END
--- Source/Application/AnlApplicationInterface.h
+++ Source/Application/AnlApplicationInterface.h
@@ -42,6 +42,8 @@ namespace Application
             // juce::Component
             void resized() override;
             void paint(juce::Graphics& g) override;
+            void lookAndFeelChanged() override;
+            void visibilityChanged() override;
 
             // juce::FileDragAndDropTarget
             bool isInterestedInFileDrag(juce::StringArray const& files) override;
@@ -54,11 +56,45 @@ namespace Application
             void applicationCommandInvoked(juce::ApplicationCommandTarget::InvocationInfo const& info) override;
             void applicationCommandListChanged() override;
 
+            class FileTable
+            : public juce::Component
+            , private juce::ListBoxModel
+            {
+            public:
+                FileTable();
+                ~FileTable() override;
+
+                // juce::Component
+                void resized() override;
+
+                std::function<void(juce::File const& file)> onFileSelected = nullptr;
+
+            private:
+                // juce::ListBoxModel
+                int getNumRows() override;
+                void paintListBoxItem(int rowNumber, juce::Graphics& g, int width, int height, bool rowIsSelected) override;
+                void returnKeyPressed(int lastRowSelected) override;
+                void listBoxItemDoubleClicked(int row, juce::MouseEvent const& event) override;
+
+                Accessor::Listener mListener;
+                juce::ListBox mListBox{"", this};
+                JUCE_LEAK_DETECTOR(FileTable)
+            };
+
             Document::Accessor::Listener mDocumentListener;
+            
             juce::TextButton mLoadFileButton{juce::translate("Load")};
-            juce::Label mLoadFileLabel{"", juce::translate("Or\nDrag & Drop\n(Audio/Document)")};
+            juce::Label mLoadFileInfo{"", ""};
+            juce::Label mLoadFileWildcard{""};
             juce::TextButton mAddTrackButton{juce::translate("Add Track")};
+            juce::Label mAddTrackInfo{""};
             juce::TextButton mLoadTemplateButton{juce::translate("Load Template")};
+            juce::Label mLoadTemplateInfo{""};
+            
+            juce::Label mSelectRecentDocument{"", juce::translate("Select Recent Document")};
+            ColouredPanel mSeparatorVertical;
+            ColouredPanel mSeparatorHorizontal;
+            FileTable mFileTable;
             bool mIsDragging{false};
         };
 
