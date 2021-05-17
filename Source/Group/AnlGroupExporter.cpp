aadf01e6402032e7ad5a46c7439b098933af4a70
Pierre Guillot <guillotpierre6@gmail.com>
2021-05-17 16:04:23 +0200

    Group::Exporter: fix trhead-safety and message

--- Source/Group/AnlGroupExporter.cpp
+++ Source/Group/AnlGroupExporter.cpp
@@ -6,28 +6,45 @@ ANALYSE_FILE_BEGIN
 
 juce::Result Group::Exporter::toImage(Accessor& accessor, Zoom::Accessor& timeZoomAccessor, juce::File const& file, int width, int height)
 {
+    juce::MessageManager::Lock lock;
+    if(!lock.tryEnter())
+    {
+        return juce::Result::fail("Invalid threaded access to model");
+    }
+    auto const name = accessor.getAttr<AttrType::name>();
+    lock.exit();
+
     if(width <= 0 || height <= 0)
     {
-        return juce::Result::fail(juce::translate("The group ANLNAME can not be exported as image because image size is not valid.").replace("ANLNAME", accessor.getAttr<AttrType::name>()));
+        return juce::Result::fail(juce::translate("The group ANLNAME can not be exported as image because image size is not valid.").replace("ANLNAME", name));
     }
     juce::TemporaryFile temp(file);
 
     auto* imageFormat = juce::ImageFileFormat::findImageFormatForFileExtension(temp.getFile());
     if(imageFormat == nullptr)
     {
-        return juce::Result::fail(juce::translate("The group ANLNAME can not be exported as image because the format of the file FLNAME is not supported.").replace("ANLNAME", accessor.getAttr<AttrType::name>().replace("FLNAME", file.getFullPathName())));
+        return juce::Result::fail(juce::translate("The group ANLNAME can not be exported as image because the format of the file FLNAME is not supported.").replace("ANLNAME", name).replace("FLNAME", file.getFullPathName()));
     }
 
     juce::FileOutputStream stream(temp.getFile());
     if(!stream.openedOk())
     {
-        return juce::Result::fail(juce::translate("The group ANLNAME can not be exported as image because the output stream of the file FLNAME cannot be opened.").replace("ANLNAME", accessor.getAttr<AttrType::name>().replace("FLNAME", file.getFullPathName())));
+        return juce::Result::fail(juce::translate("The group ANLNAME can not be exported as image because the output stream of the file FLNAME cannot be opened.").replace("ANLNAME", name).replace("FLNAME", file.getFullPathName()));
     }
 
-    Transport::Accessor transportAccessor;
-    Plot plot(accessor, transportAccessor, timeZoomAccessor);
-    plot.setSize(width, height);
-    auto const image = plot.createComponentSnapshot({0, 0, width, height});
+    auto getImage = [&]()
+    {
+        if(!lock.tryEnter())
+        {
+            return juce::Image{};
+        }
+        Transport::Accessor transportAccessor;
+        Plot plot(accessor, transportAccessor, timeZoomAccessor);
+        plot.setSize(width, height);
+        return plot.createComponentSnapshot({0, 0, width, height});
+    };
+
+    auto const image = getImage();
     if(!imageFormat->writeImageToStream(image, stream))
     {
         return juce::Result::fail(juce::translate("The group ANLNAME can not be exported as image because the output stream of the file FLNAME cannot be written.").replace("ANLNAME", accessor.getAttr<AttrType::name>().replace("FLNAME", file.getFullPathName())));
