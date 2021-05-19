6ce8d3cfe99c026ebe9ac119406964b08cc8f33f
Pierre Guillot <guillotpierre6@gmail.com>
2021-05-19 16:53:11 +0200

    Group::StateButton: fix name

--- Source/Group/AnlGroupButtonState.cpp
+++ Source/Group/AnlGroupButtonState.cpp
@@ -7,59 +7,65 @@ Group::StateButton::StateButton(Accessor& accessor)
 {
     addAndMakeVisible(mProcessingButton);
     
-    mTrackListener.onAttrChanged = [&](Track::Accessor const& acsr, Track::AttrType attribute)
+    auto updateToolTip = [&]()
+    {
+        auto const& name = mAccessor.getAttr<AttrType::name>();
+        auto const& contents = mTrackAccessors.getContents();
+        auto const valid = std::all_of(contents.cbegin(), contents.cend(), [](auto const& pair)
+                                       {
+            return pair.second.get().template getAttr<Track::AttrType::warnings>() == Track::WarningType::none;
+        });
+        
+        auto const processing = std::any_of(contents.cbegin(), contents.cend(), [](auto const& pair)
+                                            {
+            return std::get<0>(pair.second.get().template getAttr<Track::AttrType::processing>());
+        });
+        auto const rendering = std::any_of(contents.cbegin(), contents.cend(), [](auto const& pair)
+                                           {
+            return std::get<2>(pair.second.get().template getAttr<Track::AttrType::processing>());
+        });
+        if(processing && rendering)
+        {
+            setTooltip(name + ": processing and rendering...");
+            mProcessingButton.setTooltip(name + ": processing and rendering...");
+        }
+        else if(processing)
+        {
+            setTooltip(name + ": processing...");
+            mProcessingButton.setTooltip(name + ": processing...");
+        }
+        else if(rendering)
+        {
+            setTooltip(name + ": rendering...");
+            mProcessingButton.setTooltip(name + ": rendering...");
+        }
+        else if(valid)
+        {
+            setTooltip(name + ": analyses and renderings successfully completed!");
+            mProcessingButton.setTooltip(name + ": analyses and renderings successfully completed!");
+        }
+        else
+        {
+            setTooltip(name + ": analyses and renderings finished with errors!");
+            mProcessingButton.setTooltip(name + ":analyses and renderings finished with errors!");
+        }
+        
+        mProcessingButton.setActive(processing || rendering);
+        mProcessingButton.setInactiveImage(IconManager::getIcon(valid ? IconManager::IconType::checked : IconManager::IconType::alert));
+    };
+    
+    mTrackListener.onAttrChanged = [=](Track::Accessor const& acsr, Track::AttrType attribute)
     {
         juce::ignoreUnused(acsr);
         switch(attribute)
         {
-            case Track::AttrType::name:
             case Track::AttrType::processing:
             case Track::AttrType::warnings:
             {
-                auto const& contents = mTrackAccessors.getContents();
-                auto const valid = std::all_of(contents.cbegin(), contents.cend(), [](auto const& pair)
-                {
-                    return pair.second.get().template getAttr<Track::AttrType::warnings>() == Track::WarningType::none;
-                });
-                
-                auto const processing = std::any_of(contents.cbegin(), contents.cend(), [](auto const& pair)
-                {
-                    return std::get<0>(pair.second.get().template getAttr<Track::AttrType::processing>());
-                });
-                auto const rendering = std::any_of(contents.cbegin(), contents.cend(), [](auto const& pair)
-                {
-                    return std::get<2>(pair.second.get().template getAttr<Track::AttrType::processing>());
-                });
-                if(processing && rendering)
-                {
-                    setTooltip("Overview: processing and rendering...");
-                    mProcessingButton.setTooltip("Overview: processing and rendering...");
-                }
-                else if(processing)
-                {
-                    setTooltip("Overview: processing...");
-                    mProcessingButton.setTooltip("Overview: processing...");
-                }
-                else if(rendering)
-                {
-                    setTooltip("Overview: rendering...");
-                    mProcessingButton.setTooltip("Overview: rendering...");
-                }
-                else if(valid)
-                {
-                    setTooltip("Overview: analyses and renderings successfully completed!");
-                    mProcessingButton.setTooltip("Overview: analyses and renderings successfully completed!");
-                }
-                else
-                {
-                    setTooltip("Overview: analyses and renderings finished with errors!");
-                    mProcessingButton.setTooltip("Overview: analyses and renderings finished with errors!");
-                }
-                
-                mProcessingButton.setActive(processing || rendering);
-                mProcessingButton.setInactiveImage(IconManager::getIcon(valid ? IconManager::IconType::checked : IconManager::IconType::alert));
+                updateToolTip();
             }
                 break;
+            case Track::AttrType::name:
             case Track::AttrType::key:
             case Track::AttrType::description:
             case Track::AttrType::state:
@@ -76,13 +82,18 @@ Group::StateButton::StateButton(Accessor& accessor)
         }
     };
     
-    mListener.onAttrChanged = [this](class Accessor const& acsr, AttrType attribute)
+    mListener.onAttrChanged = [=, this](Accessor const& acsr, AttrType attribute)
     {
         juce::ignoreUnused(acsr);
         switch(attribute)
         {
             case AttrType::identifier:
+                break;
             case AttrType::name:
+            {
+                updateToolTip();
+            }
+                break;
             case AttrType::height:
             case AttrType::colour:
             case AttrType::expanded:
