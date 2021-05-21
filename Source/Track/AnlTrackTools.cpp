73afe3256958c73388cf4f71638cc00433778705
Pierre Guillot <guillotpierre6@gmail.com>
2021-05-21 09:58:09 +0200

    Track::Tools: add getNumBins()

--- Source/Track/AnlTrackTools.cpp
+++ Source/Track/AnlTrackTools.cpp
@@ -76,6 +76,31 @@ size_t Track::Tools::getNumColumns(Accessor const& acsr)
     return 0_z;
 }
 
+size_t Track::Tools::getNumBins(Accessor const& acsr)
+{
+    auto const& results = acsr.getAttr<AttrType::results>();
+    if(results.isEmpty() || results.getMarkers() != nullptr)
+    {
+        return 0_z;
+    }
+    if(results.getPoints() != nullptr)
+    {
+        return 1_z;
+    }
+    auto const columns = results.getColumns();
+    if(columns == nullptr || columns->empty())
+    {
+        return 0_z;
+    }
+    return std::accumulate(columns->cbegin(), columns->cend(), 0_z, [](auto const& val, auto const& channel)
+                           {
+                               return std::accumulate(channel.cbegin(), channel.cend(), val, [](auto const& rval, auto const& column)
+                                                      {
+                                                          return std::max(rval, std::get<2>(column).size());
+                                                      });
+                           });
+}
+
 float Track::Tools::valueToPixel(float value, juce::Range<double> const& valueRange, juce::Rectangle<float> const& bounds)
 {
     return (1.0f - static_cast<float>((static_cast<double>(value) - valueRange.getStart()) / valueRange.getLength())) * bounds.getHeight() + bounds.getY();
