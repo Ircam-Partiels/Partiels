dbe78cef8d2cbded95f46474237cfb51b8c626a1
Pierre Guillot <guillotpierre6@gmail.com>
2021-06-18 10:36:44 +0200

    Transport::AudioReader: format

--- Source/Transport/AnlTransportAudioReader.h
+++ Source/Transport/AnlTransportAudioReader.h
@@ -13,90 +13,88 @@ namespace Transport
     public:
         AudioReader(Accessor& accessor);
         ~AudioReader() override;
-        
+
         void setAudioFormatReader(std::unique_ptr<juce::AudioFormatReader> audioFormatReader);
-        
+
         // juce::AudioSource
         void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
         void releaseResources() override;
         void getNextAudioBlock(juce::AudioSourceChannelInfo const& bufferToFill) override;
-        
+
     private:
         // juce::Timer
         void timerCallback() override;
-        
+
         class Source
         : public juce::AudioSource
         {
         public:
             Source(std::unique_ptr<juce::AudioFormatReader> audioFormatReader);
             ~Source() override = default;
-            
+
             // juce::AudioSource
             void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
             void releaseResources() override;
             void getNextAudioBlock(juce::AudioSourceChannelInfo const& bufferToFill) override;
-            
+
             void setPlaying(bool shouldPlay);
             void setGain(float gain);
             void setStartPlayheadPosition(double position);
             void setLooping(bool shouldLoop);
             void setLoopRange(juce::Range<double> const& loopRange);
-            
+
             double getSampleRate() const;
             bool isPlaying() const;
             double getReadPlayheadPosition() const;
 
         private:
-            
             std::unique_ptr<juce::AudioFormatReader> mAudioFormatReader;
             juce::AudioFormatReaderSource mAudioFormatReaderSource;
-            
-            std::atomic<bool> mIsPlaying {false};
-            std::atomic<juce::int64> mReadPosition {0};
-            std::atomic<juce::int64> mStartPosition {0};
-            std::atomic<bool> mIsLooping {false};
-            std::atomic<juce::Range<juce::int64>> mLoopRange {};
+
+            std::atomic<bool> mIsPlaying{false};
+            std::atomic<juce::int64> mReadPosition{0};
+            std::atomic<juce::int64> mStartPosition{0};
+            std::atomic<bool> mIsLooping{false};
+            std::atomic<juce::Range<juce::int64>> mLoopRange{};
             juce::LinearSmoothedValue<float> mVolume;
             std::atomic<float> mVolumeTargetValue;
         };
-        
+
         class ResamplingSource
         : public juce::AudioSource
         {
         public:
-            
             ResamplingSource(std::unique_ptr<juce::AudioFormatReader> audioFormatReader, int numChannels);
             ~ResamplingSource() override = default;
-            
+
             // juce::AudioSource
             void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
             void releaseResources() override;
             void getNextAudioBlock(juce::AudioSourceChannelInfo const& bufferToFill) override;
-            
+
             void setPlaying(bool shouldPlay);
             void setGain(float gain);
             void setStartPlayheadPosition(double position);
             void setLooping(bool shouldLoop);
             void setLoopRange(juce::Range<double> const& loopRange);
-            
+
             bool isPlaying() const;
             double getReadPlayheadPosition() const;
-            
+
         private:
             Source mSource;
             juce::ResamplingAudioSource mResamplingAudioSource;
         };
-        
+
         Accessor& mAccessor;
         Accessor::Listener mListener;
         double mSampleRate = 44100.0;
         int mSamplesPerBlockExpected = 512;
-        
+
         AtomicManager<ResamplingSource> mSourceManager;
-        
+
         JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioReader)
     };
-}
+} // namespace Transport
 
 ANALYSE_FILE_END
