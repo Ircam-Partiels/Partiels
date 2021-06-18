c81db725d617030c46c0b4e7f71721771fbfeae0
Pierre Guillot <guillotpierre6@gmail.com>
2021-06-18 10:36:55 +0200

    test: format

--- Source/Test.cpp
+++ Source/Test.cpp
@@ -4,11 +4,11 @@
 extern "C" int main(int argc, char* argv[])
 {
     juce::ignoreUnused(argc, argv);
-    
+
     std::unique_ptr<juce::MessageManager> mm(juce::MessageManager::getInstance());
     juce::UnitTestRunner unitTestRunner;
     unitTestRunner.runAllTests();
-    
+
     int failures = 0;
     for(int i = 0; i < unitTestRunner.getNumResults(); ++i)
     {
