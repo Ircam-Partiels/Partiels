7393fbff5bdfeb721eff1839f5e4611b819b92d9
Pierre Guillot <guillotpierre6@gmail.com>
2021-06-22 10:40:08 +0200

    Zoom::Ruler: use Zoom::Grid

--- Source/Zoom/AnlZoomRuler.cpp
+++ Source/Zoom/AnlZoomRuler.cpp
@@ -2,134 +2,43 @@
 
 ANALYSE_FILE_BEGIN
 
-Zoom::Ruler::Ruler(Accessor& accessor, Orientation orientation, size_t primaryTickInterval, double tickReferenceValue, double tickPowerInterval, double divisionFactor, double maxStringWidth)
+Zoom::Ruler::Ruler(Accessor& accessor, Orientation orientation, std::function<juce::String(double)> valueToString, int maxStringWidth)
 : mAccessor(accessor)
 , mOrientation(orientation)
+, mValueToString(valueToString)
+, mMaximumStringWidth(std::max(maxStringWidth, 0))
 {
-    setMaximumStringWidth(maxStringWidth);
-    setPrimaryTickInterval(primaryTickInterval);
-    setTickReferenceValue(tickReferenceValue);
-    setTickPowerInterval(tickPowerInterval, divisionFactor);
-
     mListener.onAttrChanged = [&](Accessor const& acsr, AttrType attribute)
     {
+        juce::ignoreUnused(acsr);
         switch(attribute)
         {
             case AttrType::globalRange:
             case AttrType::minimumLength:
                 break;
+            case AttrType::anchor:
             case AttrType::visibleRange:
             {
                 repaint();
             }
             break;
-            case AttrType::anchor:
-            {
-                mZooming = std::get<0>(acsr.getAttr<AttrType::anchor>());
-                mAnchor = mFromZoomRange(std::get<1>(acsr.getAttr<AttrType::anchor>()));
-            }
-            break;
         }
     };
 
+    mGridListener.onAttrChanged = [this](Grid::Accessor const& acsr, Grid::AttrType attribute)
+    {
+        juce::ignoreUnused(acsr, attribute);
+        repaint();
+    };
+
+    mAccessor.getAcsr<AcsrType::grid>().addListener(mGridListener, NotificationType::synchronous);
     mAccessor.addListener(mListener, NotificationType::synchronous);
 }
 
 Zoom::Ruler::~Ruler()
 {
     mAccessor.removeListener(mListener);
-}
-
-void Zoom::Ruler::setPrimaryTickInterval(size_t primaryTickInterval)
-{
-    mPrimaryTickInterval = std::max(primaryTickInterval, static_cast<size_t>(0)) + 1;
-    repaint();
-}
-
-size_t Zoom::Ruler::getPrimaryTickInterval() const
-{
-    return mPrimaryTickInterval - 1;
-}
-
-void Zoom::Ruler::setTickReferenceValue(double tickReferenceValue)
-{
-    mTickReferenceValue = tickReferenceValue;
-    repaint();
-}
-
-double Zoom::Ruler::getTickReferenceValue() const
-{
-    return mTickReferenceValue;
-}
-
-void Zoom::Ruler::setTickPowerInterval(double tickPowerInterval, double divisionFactor)
-{
-    anlStrongAssert(tickPowerInterval >= 1.0);
-    anlStrongAssert(divisionFactor >= 1.0);
-    mTickPowerInterval = std::max(tickPowerInterval, 1.0);
-    mTickPowerDivisionFactor = std::max(divisionFactor, 1.0);
-    repaint();
-}
-
-double Zoom::Ruler::getTickPowerInterval() const
-{
-    return mTickPowerInterval;
-}
-
-double Zoom::Ruler::getTickPowerDivisionFactor() const
-{
-    return mTickPowerDivisionFactor;
-}
-
-void Zoom::Ruler::setMaximumStringWidth(double maxStringWidth)
-{
-    anlStrongAssert(maxStringWidth > 5.0);
-    mMaximumStringWidth = std::max(maxStringWidth, 5.0);
-
-    repaint();
-}
-
-void Zoom::Ruler::setValueAsStringMethod(std::function<juce::String(double)> valueAsStringMethod)
-{
-    mGetValueAsString = valueAsStringMethod;
-    repaint();
-}
-
-void Zoom::Ruler::setRangeConversionMethods(std::function<double(double)> fromZoomToRange, std::function<double(double)> toZoomFromRange)
-{
-    mFromZoomRange = fromZoomToRange;
-    mToZoomRange = toZoomFromRange;
-
-    repaint();
-}
-
-void Zoom::Ruler::setOrientation(Orientation orientation)
-{
-    if(mOrientation != orientation)
-    {
-        mOrientation = orientation;
-        resized();
-    }
-}
-
-Zoom::Ruler::Orientation Zoom::Ruler::getOrientation()
-{
-    return mOrientation;
-}
-
-juce::String Zoom::Ruler::valueAsStringKiloShorthand(double value)
-{
-    return std::abs(value / 1000.0) < 1.0 ? juce::String(static_cast<int>(value)) : juce::String(static_cast<int>(value / 1000.0)) + "K";
-}
-
-double Zoom::Ruler::fromZoomRangePassThrough(double input)
-{
-    return input;
-}
-
-double Zoom::Ruler::toZoomRangePassThrough(double input)
-{
-    return input;
+    mAccessor.getAcsr<AcsrType::grid>().removeListener(mGridListener);
 }
 
 void Zoom::Ruler::mouseDown(juce::MouseEvent const& event)
@@ -145,8 +54,7 @@ void Zoom::Ruler::mouseDown(juce::MouseEvent const& event)
     }
 
     auto const visibleRange = mAccessor.getAttr<AttrType::visibleRange>();
-    mInitialValueRange = {mFromZoomRange(visibleRange.getStart()), mFromZoomRange(visibleRange.getEnd())};
-
+    mInitialValueRange = visibleRange;
     if(mInitialValueRange.isEmpty())
     {
         return;
@@ -186,19 +94,18 @@ void Zoom::Ruler::mouseDown(juce::MouseEvent const& event)
             mPrevMousePos = event.position;
         }
         break;
-        case NavigationMode::select:
-            break;
         case NavigationMode::zoom:
         {
             mAccessor.setAttr<AttrType::anchor>(std::make_tuple(true, getAnchorPoint()), NotificationType::synchronous);
             event.source.enableUnboundedMouseMovement(true, false);
-
             if(onMouseDown != nullptr)
             {
                 onMouseDown(event.x);
             }
         }
         break;
+        case NavigationMode::select:
+            break;
     }
 }
 
@@ -231,11 +138,11 @@ void Zoom::Ruler::mouseDrag(juce::MouseEvent const& event)
         {
             auto const zoomDistance = static_cast<double>(isVertical ? event.getDistanceFromDragStartX() : event.getDistanceFromDragStartY());
             auto const zoomFactor = zoomDistance < 0.0 ? std::pow(1.005, std::abs(zoomDistance)) : 1.0 / std::pow(1.005, std::abs(zoomDistance));
-            auto const rangeStart = mAnchor - (mAnchor - mInitialValueRange.getStart()) * zoomFactor;
-            auto const rangeEnd = mAnchor + (mInitialValueRange.getEnd() - mAnchor) * zoomFactor;
+            auto const anchor = std::get<1>(mAccessor.getAttr<AttrType::anchor>());
+            auto const rangeStart = anchor - (anchor - mInitialValueRange.getStart()) * zoomFactor;
+            auto const rangeEnd = anchor + (mInitialValueRange.getEnd() - anchor) * zoomFactor;
 
-            mAccessor.setAttr<AttrType::visibleRange>(Range{mToZoomRange(rangeStart), mToZoomRange(rangeEnd)}, NotificationType::synchronous);
-            mAccessor.setAttr<AttrType::anchor>(std::make_tuple(true, mToZoomRange(mAnchor)), NotificationType::synchronous);
+            mAccessor.setAttr<AttrType::visibleRange>(Range{rangeStart, rangeEnd}, NotificationType::synchronous);
         }
         break;
     }
@@ -254,7 +161,7 @@ void Zoom::Ruler::mouseUp(juce::MouseEvent const& event)
     {
         case NavigationMode::translate:
         {
-            mAccessor.setAttr<AttrType::anchor>(std::make_tuple(false, mToZoomRange(mAnchor)), NotificationType::synchronous);
+            mAccessor.setAttr<AttrType::anchor>(std::make_tuple(false, std::get<1>(mAccessor.getAttr<AttrType::anchor>())), NotificationType::synchronous);
         }
         break;
         case NavigationMode::select:
@@ -262,12 +169,12 @@ void Zoom::Ruler::mouseUp(juce::MouseEvent const& event)
             mSelectedValueRange = calculateSelectedValueRange(event);
             mAccessor.setAttr<AttrType::visibleRange>(mSelectedValueRange, NotificationType::synchronous);
             auto const anchorPos = (mSelectedValueRange.getStart() + mSelectedValueRange.getEnd()) / 2.0;
-            mAccessor.setAttr<AttrType::anchor>(std::make_tuple(true, mToZoomRange(anchorPos)), NotificationType::synchronous);
+            mAccessor.setAttr<AttrType::anchor>(std::make_tuple(true, anchorPos), NotificationType::synchronous);
         }
         break;
         case NavigationMode::zoom:
         {
-            mAccessor.setAttr<AttrType::anchor>(std::make_tuple(false, mToZoomRange(mAnchor)), NotificationType::synchronous);
+            mAccessor.setAttr<AttrType::anchor>(std::make_tuple(false, std::get<1>(mAccessor.getAttr<AttrType::anchor>())), NotificationType::synchronous);
         }
         break;
     }
@@ -295,117 +202,46 @@ juce::Range<double> Zoom::Ruler::calculateSelectedValueRange(juce::MouseEvent co
 
     auto const startValue = static_cast<double>(mouseStart) / static_cast<double>(size) * mInitialValueRange.getLength() + mInitialValueRange.getStart();
     auto const endValue = static_cast<double>(mouseEnd) / static_cast<double>(size) * mInitialValueRange.getLength() + mInitialValueRange.getStart();
-    return {mToZoomRange(std::min(startValue, endValue)), mToZoomRange(std::max(startValue, endValue))};
+    return {std::min(startValue, endValue), std::max(startValue, endValue)};
 }
 
 void Zoom::Ruler::paint(juce::Graphics& g)
 {
     g.fillAll(findColour(backgroundColourId));
-    auto fromZoomRange = [this](juce::Range<double> const& range) -> juce::Range<double>
-    {
-        return {mFromZoomRange(range.getStart()), mFromZoomRange(range.getEnd())};
-    };
-
-    auto const visibleRange = mAccessor.getAttr<AttrType::visibleRange>();
-    auto const useableRange = fromZoomRange(visibleRange);
-    if(useableRange.isEmpty())
+    auto const& gridAcsr = mAccessor.getAcsr<AcsrType::grid>();
+    auto const& visibleRange = mAccessor.getAttr<AttrType::visibleRange>();
+    if(visibleRange.isEmpty())
     {
         return;
     }
 
-    auto const useableRangeLength = useableRange.getLength();
-    auto const height = getHeight();
-    auto const width = getWidth();
-    auto const isVerticallyOriented = mOrientation == Orientation::vertical;
-    auto const size = isVerticallyOriented ? height - 1 : width - 1;
-    auto const textPadding = isVerticallyOriented ? 0 : 2;
-
-    // The minimum interval depends on the font height
-    auto const font = g.getCurrentFont();
-    auto const minTickSpacing = isVerticallyOriented ? static_cast<double>(font.getHeight() + 2) : static_cast<double>(mMaximumStringWidth + textPadding * 2);
-    auto const sizeRatio = size / useableRangeLength;
-
-    // Gets the best range interval between each tick
-    auto const continuousNumTicks = std::max(1.0, std::floor(static_cast<double>(size) / minTickSpacing));
-    auto const continuousIntervalValue = useableRangeLength / continuousNumTicks;
-    auto const intervalValuePower = std::ceil(std::log(continuousIntervalValue) / std::log(mTickPowerInterval));
-    auto const discreteIntervalValueNonDivided = std::pow(mTickPowerInterval, intervalValuePower);
-
-    // Convert tick reference value into useable range
-    auto const useableTickReferenceValue = mFromZoomRange(mTickReferenceValue);
-
-    auto discreteIntervalValue = discreteIntervalValueNonDivided;
-    if(mTickPowerDivisionFactor > 1.0)
+    auto const bounds = getLocalBounds();
+    g.setColour(findColour(gridColourId));
+    if(mOrientation == Orientation::vertical)
     {
-        while(discreteIntervalValue / mTickPowerDivisionFactor * sizeRatio > minTickSpacing)
-        {
-            discreteIntervalValue /= mTickPowerDivisionFactor;
-        }
+        Grid::paintVertical(g, gridAcsr, visibleRange, bounds, mValueToString, Grid::Justification::left);
+    }
+    else
+    {
+
+        Grid::paintHorizontal(g, gridAcsr, visibleRange, bounds, mValueToString, mMaximumStringWidth, Grid::Justification::bottom);
     }
 
-    // Gets the first value and the number of ticks to display
-    auto const firstValue = std::floor((useableRange.getStart() - useableTickReferenceValue) / discreteIntervalValue) * discreteIntervalValue + useableTickReferenceValue;
-    auto const discreteNumTick = static_cast<size_t>(std::ceil(useableRangeLength / discreteIntervalValue)) + 1;
-
-    // Gets the ticks width and text width
-    auto const primaryTickSpacing = discreteIntervalValue * static_cast<double>(mPrimaryTickInterval);
-    auto const maxTickLength = isVerticallyOriented ? getWidth() - 2 : getHeight() - 2;
-    auto const primaryTickLength = static_cast<float>(std::round(maxTickLength * 0.5));
-    auto const secondaryTickLength = static_cast<float>(std::round(maxTickLength * 0.25));
-
-    g.setFont(font);
-    auto const primaryTickEpsilon = visibleRange.getLength() / std::max(static_cast<double>(isVerticallyOriented ? height - 2 : width - 2), 1.0);
-    for(size_t tickIndex = 0; tickIndex <= discreteNumTick; ++tickIndex)
-    {
-        auto const currentValue = firstValue + static_cast<double>(tickIndex) * discreteIntervalValue;
-        auto const isPrimaryTick = std::abs(std::remainder(currentValue - useableTickReferenceValue, primaryTickSpacing)) < primaryTickEpsilon;
-
-        auto const tickLengh = isPrimaryTick ? primaryTickLength : secondaryTickLength;
-        auto const position = isVerticallyOriented ? static_cast<float>(size - std::round((currentValue - useableRange.getStart()) * sizeRatio) + 0.5) : static_cast<float>(std::round((currentValue - useableRange.getStart()) * sizeRatio) + 0.5);
-        g.setColour(findColour(tickColourId));
-
-        if(isVerticallyOriented)
-        {
-            g.drawLine(1.f, position, tickLengh, position);
-        }
-        else
-        {
-            g.drawVerticalLine(static_cast<int>(std::floor(position)), static_cast<float>(height) - tickLengh, static_cast<float>(height));
-        }
-
-        if(isPrimaryTick && position < static_cast<float>(size + 1))
-        {
-            g.setColour(findColour(textColourId));
-
-            auto const displayValue = mToZoomRange(currentValue);
-            auto const valueText = (mGetValueAsString != nullptr) ? mGetValueAsString(displayValue) : juce::String(displayValue);
-
-            if(isVerticallyOriented)
-            {
-                if(font.getStringWidth(valueText) < width - 1)
-                {
-                    g.drawSingleLineText(valueText, 1, static_cast<int>(std::ceil(position - static_cast<float>(font.getDescent()))), juce::Justification::left);
-                }
-            }
-            else
-            {
-                g.drawSingleLineText(valueText, static_cast<int>(std::floor(position)) + textPadding, height - 1, juce::Justification::left);
-            }
-        }
-    }
-
-    if(mZooming)
+    auto const width = bounds.getWidth();
+    auto const height = bounds.getHeight();
+    if(std::get<0>(mAccessor.getAttr<AttrType::anchor>()))
     {
         g.setColour(findColour(anchorColourId, true));
-        auto const anchor = (mAnchor - useableRange.getStart()) / useableRange.getLength();
-        if(isVerticallyOriented)
+        auto const anchor = std::get<1>(mAccessor.getAttr<AttrType::anchor>());
+        auto const anchorRatio = (anchor - visibleRange.getStart()) / visibleRange.getLength();
+        if(mOrientation == Orientation::vertical)
         {
-            auto const anchorPosition = static_cast<int>((1.0f - anchor) * static_cast<float>(height));
+            auto const anchorPosition = static_cast<int>(std::round((1.0f - anchorRatio) * static_cast<float>(height)));
             g.drawHorizontalLine(anchorPosition, 0.0f, static_cast<float>(width));
         }
         else
         {
-            auto const anchorPosition = static_cast<int>(anchor * static_cast<float>(width));
+            auto const anchorPosition = static_cast<int>(std::round(anchorRatio * static_cast<float>(width)));
             g.drawVerticalLine(anchorPosition, 0.0f, static_cast<float>(height));
         }
     }
@@ -413,10 +249,9 @@ void Zoom::Ruler::paint(juce::Graphics& g)
     {
         auto getSelectionRectangle = [&]() -> juce::Rectangle<float>
         {
-            auto const usableValueRange = fromZoomRange(mSelectedValueRange);
-            auto const start = static_cast<float>((usableValueRange.getStart() - useableRange.getStart()) / useableRange.getLength());
-            auto const end = static_cast<float>((usableValueRange.getEnd() - useableRange.getStart()) / useableRange.getLength());
-            if(isVerticallyOriented)
+            auto const start = static_cast<float>((mSelectedValueRange.getStart() - visibleRange.getStart()) / visibleRange.getLength());
+            auto const end = static_cast<float>((mSelectedValueRange.getEnd() - visibleRange.getStart()) / visibleRange.getLength());
+            if(mOrientation == Orientation::vertical)
             {
                 return {0.0f, (1.0f - end) * static_cast<float>(height), static_cast<float>(width), (end - start) * static_cast<float>(height)};
             }
--- Source/Zoom/AnlZoomRuler.h
+++ Source/Zoom/AnlZoomRuler.h
@@ -14,8 +14,7 @@ namespace Zoom
         enum ColourIds : int
         {
               backgroundColourId = 0x2010000
-            , tickColourId
-            , textColourId
+            , gridColourId
             , anchorColourId
             , selectionColourId
         };
@@ -27,54 +26,13 @@ namespace Zoom
         };
         // clang-format on
 
-        Ruler(Accessor& accessor, Orientation orientation, size_t primaryTickInterval = 3, double tickReferenceValue = 0.0, double tickPowerInterval = 2.0, double divisionFactor = 1.0, double maxStringWidth = 25.0);
+        Ruler(Accessor& accessor, Orientation const orientation, std::function<juce::String(double)> valueToString = nullptr, int const maxStringWidth = 70);
 
         ~Ruler() override;
-
-        //! @brief Sets the power and division factors that determine the space between each tick
-        void setTickPowerInterval(double tickPowerInterval, double divisionFactor = 1.0);
-
-        //! @brief Sets the power factor that determine the space between each tick
-        double getTickPowerInterval() const;
-
-        //! @brief Sets the division factor that determine the space between each tick
-        double getTickPowerDivisionFactor() const;
-
-        //! @brief Sets number of ticks between each primary tick
-        void setPrimaryTickInterval(size_t primaryTickInterval);
-
-        //! @brief Gets number of ticks between each primary tick
-        size_t getPrimaryTickInterval() const;
-
-        //! @brief Sets the tick reference value that is used to determine the value of other ticks
-        void setTickReferenceValue(double tickReferenceValue);
-
-        //! @brief Gets the tick reference value that is used to determine the value of other ticks
-        double getTickReferenceValue() const;
-
-        //!@brief Sets the maximum pixel width needed for to be value represented as a string. This is only used in the case of horizontally oriented rulers
-        void setMaximumStringWidth(double maxStringWidth);
-
-        //!@brief Sets the visual orientation of the rule. To be vertical or horizontal.
-        void setOrientation(Orientation orientation);
-
-        //!@brief Gets the orientation that is used to determine how to draw the ruler
-        Orientation getOrientation();
 
         std::function<void()> onDoubleClick = nullptr;
         std::function<void(int)> onMouseDown = nullptr;
         std::function<void(void)> onRightClick = nullptr;
-
-        //!@brief Sets the method used for formatting and printing a specific value as a string
-        void setValueAsStringMethod(std::function<juce::String(double)> valueAsStringMethod);
-
-        //! @brief Sets the methods used to convert between Zoom range and a custom range
-        void setRangeConversionMethods(std::function<double(double)> fromZoomToRange, std::function<double(double)> toZoomFromRange);
-
-        static juce::String valueAsStringKiloShorthand(double);
-
-        static double fromZoomRangePassThrough(double);
-        static double toZoomRangePassThrough(double);
 
         // Juce::Component
         void mouseDown(juce::MouseEvent const& event) override;
@@ -85,11 +43,6 @@ namespace Zoom
 
     private:
         juce::Range<double> calculateSelectedValueRange(juce::MouseEvent const& event);
-
-        std::function<double(double)> mFromZoomRange = fromZoomRangePassThrough;
-        std::function<double(double)> mToZoomRange = toZoomRangePassThrough;
-
-        std::function<juce::String(double)> mGetValueAsString = valueAsStringKiloShorthand;
 
         // clang-format off
         enum class NavigationMode
@@ -102,20 +55,15 @@ namespace Zoom
 
         Accessor& mAccessor;
         Accessor::Listener mListener{typeid(*this).name()};
-
-        size_t mPrimaryTickInterval = 3;
-        double mTickReferenceValue = 0.0;
-        double mTickPowerInterval = 0.0;
-        double mTickPowerDivisionFactor = 1.0;
-        double mMaximumStringWidth = 60.0;
+        Grid::Accessor::Listener mGridListener{typeid(*this).name()};
+        Orientation const mOrientation;
+        std::function<juce::String(double)> mValueToString;
+        int const mMaximumStringWidth = 60.0;
 
         juce::Range<double> mInitialValueRange;
         juce::Range<double> mSelectedValueRange{0.0, 0.0};
-        bool mZooming = false;
-        double mAnchor = 0.0;
         juce::Point<float> mPrevMousePos{0.0, 0.0};
 
-        Orientation mOrientation;
         NavigationMode mNavigationMode{NavigationMode::zoom};
     };
 } // namespace Zoom
