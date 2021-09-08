19f5bd6569eb6fa3e34bafafa9dde301217db5a1
Pierre Guillot <guillotpierre6@gmail.com>
2021-09-08 12:05:50 +0200

    SdifConverter: remve Panel class

--- Source/Misc/AnlSdifConverter.h
+++ Source/Misc/AnlSdifConverter.h
@@ -15,62 +15,6 @@ namespace SdifConverter
 
     juce::Result toJson(juce::File const& inputFile, juce::File const& outputFile, uint32_t frameId, uint32_t matrixId, std::optional<size_t> row, std::optional<size_t> column, std::optional<nlohmann::json> const& extraInfo);
     juce::Result fromJson(juce::File const& inputFile, juce::File const& outputFile, uint32_t frameId, uint32_t matrixId, std::optional<juce::String> columnName);
-
-    class Panel
-    : public FloatingWindowContainer
-    , public juce::FileDragAndDropTarget
-    {
-    public:
-        Panel();
-        ~Panel() override = default;
-
-        // juce::Component
-        void resized() override;
-        void paintOverChildren(juce::Graphics& g) override;
-        void lookAndFeelChanged() override;
-        void parentHierarchyChanged() override;
-
-        // juce::FileDragAndDropTarget
-        bool isInterestedInFileDrag(juce::StringArray const& files) override;
-        void fileDragEnter(juce::StringArray const& files, int x, int y) override;
-        void fileDragExit(juce::StringArray const& files) override;
-        void filesDropped(juce::StringArray const& files, int x, int y) override;
-
-    private:
-        void setFile(juce::File const& file);
-
-        void selectedFrameUpdated();
-        void selectedMatrixUpdated();
-        void selectedRowColumnUpdated();
-
-        void exportToJson();
-        void exportToSdif();
-
-        PropertyTextButton mPropertyOpen;
-        PropertyText mPropertyName;
-
-        PropertyText mPropertyToSdifFrame;
-        PropertyText mPropertyToSdifMatrix;
-        PropertyText mPropertyToSdifColName;
-        PropertyTextButton mPropertyToSdifExport;
-
-        PropertyList mPropertyToJsonFrame;
-        PropertyList mPropertyToJsonMatrix;
-        PropertyList mPropertyToJsonRow;
-        PropertyList mPropertyToJsonColumn;
-        PropertyText mPropertyToJsonUnit;
-        PropertyNumber mPropertyToJsonMinValue;
-        PropertyNumber mPropertyToJsonMaxValue;
-        PropertyTextButton mPropertyToJsonExport;
-        juce::TextEditor mInfos;
-
-        juce::File mFile;
-        std::map<uint32_t, std::map<uint32_t, matrix_size_t>> mEntries;
-        std::vector<uint32_t> mFrameSigLinks;
-        std::vector<uint32_t> mMatrixSigLinks;
-        bool mFileIsDragging{false};
-        std::unique_ptr<juce::FileChooser> mFileChooser;
-    };
 } // namespace SdifConverter
 
 ANALYSE_FILE_END
