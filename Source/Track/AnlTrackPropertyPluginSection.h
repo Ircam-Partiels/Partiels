2d8c7a338a39d190f7df718b75b1c1db816090a1
Pierre Guillot <pierre.guillot@ircam.fr>
2022-03-02 11:15:47 +0100

    Track::PropertyPluginSection: add juce::translate()

--- Source/Track/AnlTrackPropertyPluginSection.h
+++ Source/Track/AnlTrackPropertyPluginSection.h
@@ -22,11 +22,11 @@ namespace Track
         Accessor& mAccessor{mDirector.getAccessor()};
         Accessor::Listener mListener{typeid(*this).name()};
 
-        PropertyLabel mPropertyPluginName{"Name", "The name of the plugin"};
-        PropertyLabel mPropertyPluginFeature{"Feature", "The feature of the plugin"};
-        PropertyLabel mPropertyPluginMaker{"Maker", "The maker of the plugin"};
-        PropertyLabel mPropertyPluginVersion{"Version", "The version of the plugin"};
-        PropertyLabel mPropertyPluginCategory{"Category", "The category of the plugin"};
+        PropertyLabel mPropertyPluginName{juce::translate("Name"), juce::translate("The name of the plugin")};
+        PropertyLabel mPropertyPluginFeature{juce::translate("Feature"), juce::translate("The feature of the plugin")};
+        PropertyLabel mPropertyPluginMaker{juce::translate("Maker"), juce::translate("The maker of the plugin")};
+        PropertyLabel mPropertyPluginVersion{juce::translate("Version"), juce::translate("The version of the plugin")};
+        PropertyLabel mPropertyPluginCategory{juce::translate("Category"), juce::translate("The category of the plugin")};
         juce::TextEditor mPropertyPluginDetails;
     };
 } // namespace Track
