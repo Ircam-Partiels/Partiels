9008cdd3dc8831addb6aa9007f057f3984a484ec
Pierre Guillot <guillotpierre6@gmail.com>
2022-04-19 09:40:09 +0200

    Track::Result: add Modifier

---
+++ Source/Track/Result/AnlTrackResultModifier.cpp
@@ -0,0 +1,519 @@
+#include "AnlTrackResultModifier.h"
+
+ANALYSE_FILE_BEGIN
+
+namespace
+{
+    void showAccessWarning()
+    {
+        auto const options = juce::MessageBoxOptions()
+                                 .withIconType(juce::AlertWindow::WarningIcon)
+                                 .withTitle(juce::translate("Results cannot be accessed!"))
+                                 .withMessage(juce::translate("The results are already used by another process and cannot be modified. Please, wait for a valid access before modifying results."))
+                                 .withButton(juce::translate("Ok"));
+        juce::AlertWindow::showAsync(options, nullptr);
+    }
+} // namespace
+
+std::set<size_t> Track::Result::Modifier::getIndices(CopiedData const& data)
+{
+    auto const generateIndices = [&](auto const& results)
+    {
+        std::set<size_t> indices;
+        for(auto const& result : results)
+        {
+            indices.insert(result.first);
+        }
+        return indices;
+    };
+
+    if(auto const* markersData = std::get_if<std::map<size_t, Data::Marker>>(&data))
+    {
+        return generateIndices(*markersData);
+    }
+    else if(auto const* pointsData = std::get_if<std::map<size_t, Data::Point>>(&data))
+    {
+        return generateIndices(*pointsData);
+    }
+    else if(auto const* columnsData = std::get_if<std::map<size_t, Data::Column>>(&data))
+    {
+        return generateIndices(*columnsData);
+    }
+    else
+    {
+        return {};
+    }
+}
+
+juce::Range<double> Track::Result::Modifier::getTimeRange(CopiedData const& data)
+{
+    auto const getRange = [&](auto const& results) -> juce::Range<double>
+    {
+        if(results.empty())
+        {
+            return {};
+        }
+        return {std::get<0_z>(results.cbegin()->second), std::get<0_z>(results.crbegin()->second)};
+    };
+
+    if(auto const* markersData = std::get_if<std::map<size_t, Data::Marker>>(&data))
+    {
+        return getRange(*markersData);
+    }
+    else if(auto const* pointsData = std::get_if<std::map<size_t, Data::Point>>(&data))
+    {
+        return getRange(*pointsData);
+    }
+    else if(auto const* columnsData = std::get_if<std::map<size_t, Data::Column>>(&data))
+    {
+        return getRange(*columnsData);
+    }
+    else
+    {
+        return {};
+    }
+}
+
+Track::Result::Modifier::CopiedData Track::Result::Modifier::duplicateFrames(CopiedData const& data, juce::Range<double> const& range, size_t destinationIndex, double destinationTime)
+{
+    auto copyFrames = [&](auto const& results) -> CopiedData
+    {
+        if(results.empty())
+        {
+            return {};
+        }
+        using result_type = typename std::remove_const<typename std::remove_reference<decltype(results)>::type>::type;
+        auto const indexDifference = static_cast<long>(destinationIndex) - static_cast<long>(results.cbegin()->first);
+        auto const timeDifference = destinationTime - range.getStart();
+        result_type map;
+        for(auto const& pair : results)
+        {
+            auto frame = pair.second;
+            std::get<0_z>(frame) += timeDifference;
+            auto const index = static_cast<size_t>(static_cast<long>(pair.first) + indexDifference);
+            map[index] = std::move(frame);
+        }
+        return map;
+    };
+
+    if(auto const* markersData = std::get_if<std::map<size_t, Data::Marker>>(&data))
+    {
+        return copyFrames(*markersData);
+    }
+    else if(auto const* pointsData = std::get_if<std::map<size_t, Data::Point>>(&data))
+    {
+        return copyFrames(*pointsData);
+    }
+    else if(auto const* columnsData = std::get_if<std::map<size_t, Data::Column>>(&data))
+    {
+        return copyFrames(*columnsData);
+    }
+    return {};
+}
+
+std::optional<double> Track::Result::Modifier::getTime(Accessor const& accessor, size_t const channel, size_t const index)
+{
+    auto const findTime = [&](auto const& result) -> std::optional<double>
+    {
+        if(channel >= result.size())
+        {
+            return {};
+        }
+        auto const& channelResults = result.at(channel);
+        if(channelResults.empty())
+        {
+            return {};
+        }
+        return std::get<0_z>(channelResults.at(index));
+    };
+
+    auto const& results = accessor.getAttr<AttrType::results>();
+    auto const access = results.getReadAccess();
+    if(!static_cast<bool>(access))
+    {
+        showAccessWarning();
+        return {};
+    }
+    if(auto const markers = results.getMarkers())
+    {
+        return findTime(*markers);
+    }
+    if(auto const points = results.getPoints())
+    {
+        return findTime(*points);
+    }
+    if(auto const columns = results.getColumns())
+    {
+        return findTime(*columns);
+    }
+    return {};
+}
+
+std::optional<size_t> Track::Result::Modifier::getIndex(Accessor const& accessor, size_t const channel, double time)
+{
+    auto const findIndex = [&](auto const& results) -> std::optional<size_t>
+    {
+        if(channel >= results.size())
+        {
+            return {};
+        }
+        auto& channelFrames = results.at(channel);
+        auto start = Result::Data::findFirstAt(channelFrames, time);
+        if(start == channelFrames.cend())
+        {
+            return channelFrames.size();
+        }
+        return static_cast<size_t>(std::distance(channelFrames.cbegin(), std::get<0_z>(*start) < time ? std::next(start) : start));
+    };
+
+    auto const& results = accessor.getAttr<AttrType::results>();
+    auto const access = results.getReadAccess();
+    if(!static_cast<bool>(access))
+    {
+        showAccessWarning();
+        return {};
+    }
+    if(auto markers = results.getMarkers())
+    {
+        return findIndex(*markers);
+    }
+    if(auto points = results.getPoints())
+    {
+        return findIndex(*points);
+    }
+    if(auto columns = results.getColumns())
+    {
+        return findIndex(*columns);
+    }
+    return {};
+}
+
+std::set<size_t> Track::Result::Modifier::getIndices(Accessor const& accessor, size_t const channel, juce::Range<double> const& range)
+{
+    auto const generateIndices = [&](auto const& results) -> std::set<size_t>
+    {
+        if(channel >= results.size())
+        {
+            return {};
+        }
+        auto& channelFrames = results.at(channel);
+        auto start = Result::Data::findFirstAt(channelFrames, range.getStart());
+        if(start != channelFrames.cend() && std::get<0_z>(*start) < range.getStart())
+        {
+            start = std::next(start);
+        }
+        if(start == channelFrames.cend())
+        {
+            return {};
+        }
+        std::set<size_t> indices;
+        auto position = static_cast<size_t>(std::distance(channelFrames.cbegin(), start));
+        auto end = Result::Data::findFirstAt(channelFrames, range.getEnd());
+        if(end != channelFrames.cend() && std::get<0_z>(*end) < range.getEnd())
+        {
+            end = std::next(end);
+        }
+        while(start != end)
+        {
+            indices.insert(position++);
+            start = std::next(start);
+        }
+        return indices;
+    };
+
+    auto const& results = accessor.getAttr<AttrType::results>();
+    auto const access = results.getReadAccess();
+    if(!static_cast<bool>(access))
+    {
+        showAccessWarning();
+        return {};
+    }
+    if(auto markers = results.getMarkers())
+    {
+        return generateIndices(*markers);
+    }
+    if(auto points = results.getPoints())
+    {
+        return generateIndices(*points);
+    }
+    if(auto columns = results.getColumns())
+    {
+        return generateIndices(*columns);
+    }
+    return {};
+}
+
+Track::Result::Modifier::CopiedData Track::Result::Modifier::copyFrames(Accessor const& accessor, size_t const channel, std::set<size_t> const& indices)
+{
+    auto const getData = [&](auto& results)
+    {
+        using result_type = typename std::remove_reference<decltype(results)>::type::value_type;
+        using data_type = typename result_type::value_type;
+        std::map<size_t, data_type> map;
+        if(channel >= results.size())
+        {
+            return map;
+        }
+        auto const& channelFrames = results[channel];
+        for(auto it = indices.cbegin(); it != indices.cend(); ++it)
+        {
+            if(*it < channelFrames.size())
+            {
+                map[*it] = channelFrames[*it];
+            }
+        }
+        return map;
+    };
+
+    auto const& results = accessor.getAttr<AttrType::results>();
+    auto const access = results.getReadAccess();
+    if(!static_cast<bool>(access))
+    {
+        showAccessWarning();
+        return {};
+    }
+    if(auto markers = results.getMarkers())
+    {
+        return getData(*markers);
+    }
+    if(auto points = results.getPoints())
+    {
+        return getData(*points);
+    }
+    if(auto columns = results.getColumns())
+    {
+        return getData(*columns);
+    }
+    return {};
+}
+
+bool Track::Result::Modifier::insertFrames(Accessor& accessor, size_t const channel, CopiedData const& data, juce::String const& commit)
+{
+    auto const applyChange = [&](auto& results, auto const& newData)
+    {
+        if(channel >= results.size())
+        {
+            return false;
+        }
+        auto& channelFrames = results[channel];
+        for(auto it = newData.cbegin(); it != newData.cend(); ++it)
+        {
+            auto constexpr maxTime = std::numeric_limits<double>::max();
+            auto const index = it->first;
+            auto const previousTime = index > 0_z && index - 1_z < channelFrames.size() ? std::get<0_z>(channelFrames[index - 1_z]) + std::get<1_z>(channelFrames[index - 1_z]) : 0.0;
+            auto const nextTime = index < channelFrames.size() ? std::get<0_z>(channelFrames[index]) : maxTime;
+            MiscWeakAssert(nextTime >= previousTime);
+            auto value = it->second;
+            std::get<0_z>(value) = std::clamp(std::get<0_z>(value), previousTime, std::max(nextTime, previousTime));
+            std::get<1_z>(value) = std::min(std::get<1_z>(value), nextTime - std::get<0_z>(value));
+            channelFrames.insert(channelFrames.begin() + static_cast<long>(index), value);
+        }
+        return true;
+    };
+
+    auto const getAccessAndParse = [&](auto& results)
+    {
+        auto const access = results.getWriteAccess();
+        if(!static_cast<bool>(access))
+        {
+            showAccessWarning();
+            return false;
+        }
+        if(auto markers = results.getMarkers())
+        {
+            if(auto const* markersData = std::get_if<std::map<size_t, Data::Marker>>(&data))
+            {
+                return applyChange(*markers, *markersData);
+            }
+            return false;
+        }
+        if(auto points = results.getPoints())
+        {
+            if(auto const* pointsData = std::get_if<std::map<size_t, Data::Point>>(&data))
+            {
+                return applyChange(*points, *pointsData);
+            }
+            return false;
+        }
+        if(auto columns = results.getColumns())
+        {
+            if(auto const* columnsData = std::get_if<std::map<size_t, Data::Column>>(&data))
+            {
+                return applyChange(*columns, *columnsData);
+            }
+            return false;
+        }
+        return false;
+    };
+
+    auto results(accessor.getAttr<AttrType::results>());
+    if(getAccessAndParse(results))
+    {
+        auto file = accessor.getAttr<AttrType::file>();
+        file.commit = commit;
+        accessor.setAttr<AttrType::results>(results, NotificationType::synchronous);
+        accessor.setAttr<AttrType::file>(file, NotificationType::synchronous);
+        return true;
+    }
+    return false;
+}
+
+bool Track::Result::Modifier::eraseFrames(Accessor& accessor, size_t const channel, std::set<size_t> const& indices, juce::String const& commit)
+{
+    if(indices.empty())
+    {
+        return true;
+    }
+
+    auto const applyChange = [&](auto& results)
+    {
+        if(channel >= results.size())
+        {
+            return false;
+        }
+        auto& channelFrames = results[channel];
+        auto changed = false;
+        for(auto it = indices.crbegin(); it != indices.crend(); ++it)
+        {
+            auto const index = *it;
+            if(index < channelFrames.size())
+            {
+                channelFrames.erase(channelFrames.begin() + static_cast<long>(index));
+                changed = true;
+            }
+        }
+        return changed;
+    };
+
+    auto const getAccessAndParse = [&](auto& results)
+    {
+        auto const access = results.getWriteAccess();
+        if(!static_cast<bool>(access))
+        {
+            showAccessWarning();
+            return false;
+        }
+        if(auto markers = results.getMarkers())
+        {
+            return applyChange(*markers);
+        }
+        if(auto points = results.getPoints())
+        {
+            return applyChange(*points);
+        }
+        if(auto columns = results.getColumns())
+        {
+            return applyChange(*columns);
+        }
+        return false;
+    };
+
+    auto results(accessor.getAttr<AttrType::results>());
+    if(getAccessAndParse(results))
+    {
+        auto file = accessor.getAttr<AttrType::file>();
+        file.commit = commit;
+        accessor.setAttr<AttrType::results>(results, NotificationType::synchronous);
+        accessor.setAttr<AttrType::file>(file, NotificationType::synchronous);
+        return true;
+    }
+    return false;
+}
+
+Track::Result::Modifier::ActionBase::ActionBase(Accessor& accessor, size_t const channel)
+: mAccessor(accessor)
+, mChannel(channel)
+, mCurrentCommit(accessor.getAttr<AttrType::file>().commit)
+, mNewCommit(juce::Uuid().toString())
+{
+}
+
+Track::Result::Modifier::ActionErase::ActionErase(Accessor& accessor, Transport::Accessor& transportAcsr, size_t const channel)
+: ActionBase(accessor, channel)
+, mTransportAccessor(transportAcsr)
+, mSavedSelection(mTransportAccessor.getAttr<Transport::AttrType::selection>())
+, mSavedData(copyFrames(accessor, channel, getIndices(accessor, channel, mSavedSelection)))
+{
+}
+
+bool Track::Result::Modifier::ActionErase::perform()
+{
+    if(eraseFrames(mAccessor, mChannel, getIndices(mSavedData), mNewCommit))
+    {
+        mTransportAccessor.setAttr<Transport::AttrType::selection>(mSavedSelection, NotificationType::synchronous);
+        return true;
+    }
+    MiscWeakAssert(false);
+    return false;
+}
+
+bool Track::Result::Modifier::ActionErase::undo()
+{
+    if(insertFrames(mAccessor, mChannel, mSavedData, mCurrentCommit))
+    {
+        mTransportAccessor.setAttr<Transport::AttrType::selection>(mSavedSelection, NotificationType::synchronous);
+        return true;
+    }
+    MiscWeakAssert(false);
+    return false;
+}
+
+Track::Result::Modifier::ActionPaste::ActionPaste(Accessor& accessor, Transport::Accessor& transportAcsr, size_t const channel, CopiedData const& data, juce::Range<double> const& range)
+: ActionBase(accessor, channel)
+, mTransportAccessor(transportAcsr)
+, mSavedSelection(transportAcsr.getAttr<Transport::AttrType::selection>())
+, mDestinationSelection(range.movedToStartAt(transportAcsr.getAttr<Transport::AttrType::startPlayhead>()))
+, mSavedData(copyFrames(accessor, channel, getIndices(accessor, channel, mDestinationSelection)))
+, mCopyIndex(getIndex(accessor, channel, mDestinationSelection.getStart()))
+, mCopiedData(duplicateFrames(data, range, mCopyIndex.has_value() ? *mCopyIndex : 0_z, mDestinationSelection.getStart()))
+{
+}
+
+bool Track::Result::Modifier::ActionPaste::perform()
+{
+    if(eraseFrames(mAccessor, mChannel, getIndices(mSavedData), mNewCommit))
+    {
+        if(insertFrames(mAccessor, mChannel, mCopiedData, mNewCommit))
+        {
+            mTransportAccessor.setAttr<Transport::AttrType::selection>(mDestinationSelection, NotificationType::synchronous);
+            return true;
+        }
+        else
+        {
+            if(!insertFrames(mAccessor, mChannel, mSavedData, mCurrentCommit))
+            {
+                MiscWeakAssert(false);
+            }
+        }
+        mTransportAccessor.setAttr<Transport::AttrType::selection>(mSavedSelection, NotificationType::synchronous);
+    }
+    return false;
+}
+
+bool Track::Result::Modifier::ActionPaste::undo()
+{
+    if(eraseFrames(mAccessor, mChannel, getIndices(mCopiedData), mCurrentCommit))
+    {
+        if(insertFrames(mAccessor, mChannel, mSavedData, mCurrentCommit))
+        {
+            mTransportAccessor.setAttr<Transport::AttrType::selection>(mSavedSelection, NotificationType::synchronous);
+            return true;
+        }
+        else
+        {
+            MiscWeakAssert(false);
+            if(!insertFrames(mAccessor, mChannel, mCopiedData, mNewCommit))
+            {
+                MiscWeakAssert(false);
+            }
+        }
+        mTransportAccessor.setAttr<Transport::AttrType::selection>(mDestinationSelection, NotificationType::synchronous);
+    }
+    else
+    {
+        MiscWeakAssert(false);
+    }
+    return false;
+}
+
+ANALYSE_FILE_END
---
+++ Source/Track/Result/AnlTrackResultModifier.h
@@ -0,0 +1,147 @@
+#pragma once
+
+#include "../AnlTrackDirector.h"
+
+ANALYSE_FILE_BEGIN
+
+namespace Track
+{
+    namespace Result
+    {
+        namespace Modifier
+        {
+            using CopiedData = std::variant<std::map<size_t, Data::Marker>, std::map<size_t, Data::Point>, std::map<size_t, Data::Column>>;
+
+            std::set<size_t> getIndices(CopiedData const& data);
+            juce::Range<double> getTimeRange(CopiedData const& data);
+            CopiedData duplicateFrames(CopiedData const& data, juce::Range<double> const& range, size_t destinationIndex, double destinationTime);
+
+            std::optional<double> getTime(Accessor const& accessor, size_t const channel, size_t index);
+            std::optional<size_t> getIndex(Accessor const& accessor, size_t const channel, double time);
+            std::set<size_t> getIndices(Accessor const& accessor, size_t const channel, juce::Range<double> const& range);
+            CopiedData copyFrames(Accessor const& accessor, size_t const channel, std::set<size_t> const& indices);
+            bool insertFrames(Accessor& accessor, size_t const channel, CopiedData const& data, juce::String const& commit);
+            bool eraseFrames(Accessor& accessor, size_t const channel, std::set<size_t> const& indices, juce::String const& commit);
+
+            template <int D, typename T>
+            bool updateFrame(Accessor& accessor, size_t const channel, size_t const index, juce::String const& commit, T const fn)
+            {
+                auto const applyChange = [&](auto& resultPtr)
+                {
+                    auto& results = *resultPtr;
+                    if(channel >= results.size())
+                    {
+                        return false;
+                    }
+                    auto& channelFrames = results[channel];
+                    if(index >= channelFrames.size())
+                    {
+                        return false;
+                    }
+                    return fn(channelFrames[index]);
+                };
+
+                auto const getAccessAndParse = [&](auto& results)
+                {
+                    auto const access = results.getWriteAccess();
+                    if(!static_cast<bool>(access))
+                    {
+                        auto const options = juce::MessageBoxOptions()
+                                                 .withIconType(juce::AlertWindow::WarningIcon)
+                                                 .withTitle(juce::translate("Results cannot be accessed!"))
+                                                 .withMessage(juce::translate("The results are already used by another process and cannot be modified. Please, wait for a valid access before modifying results."))
+                                                 .withButton(juce::translate("Ok"));
+                        juce::AlertWindow::showAsync(options, nullptr);
+                        return false;
+                    }
+                    if constexpr(static_cast<bool>(D & Data::Type::marker))
+                    {
+                        if(auto markers = results.getMarkers())
+                        {
+                            return applyChange(markers);
+                        }
+                    }
+                    if constexpr(static_cast<bool>(D & Data::Type::point))
+                    {
+                        if(auto points = results.getPoints())
+                        {
+                            return applyChange(points);
+                        }
+                    }
+                    if constexpr(static_cast<bool>(D & Data::Type::column))
+                    {
+                        if(auto columns = results.getColumns())
+                        {
+                            return applyChange(columns);
+                        }
+                    }
+                    return false;
+                };
+
+                auto results(accessor.getAttr<AttrType::results>());
+                if(getAccessAndParse(results))
+                {
+                    auto file = accessor.getAttr<AttrType::file>();
+                    file.commit = commit;
+                    accessor.setAttr<AttrType::results>(results, NotificationType::synchronous);
+                    accessor.setAttr<AttrType::file>(file, NotificationType::synchronous);
+                    return true;
+                }
+                return false;
+            }
+
+            class ActionBase
+            : public juce::UndoableAction
+            {
+            public:
+                ActionBase(Accessor& accessor, size_t const channel);
+                ~ActionBase() override = default;
+
+            protected:
+                Accessor& mAccessor;
+                size_t const mChannel;
+                juce::String const mCurrentCommit;
+                juce::String const mNewCommit;
+            };
+
+            class ActionErase
+            : public ActionBase
+            {
+            public:
+                ActionErase(Accessor& accessor, Transport::Accessor& transportAcsr, size_t const channel);
+                ~ActionErase() override = default;
+
+                // juce::UndoableAction
+                bool perform() override;
+                bool undo() override;
+
+            private:
+                Transport::Accessor& mTransportAccessor;
+                juce::Range<double> const mSavedSelection;
+                Modifier::CopiedData const mSavedData;
+            };
+
+            class ActionPaste
+            : public ActionBase
+            {
+            public:
+                ActionPaste(Accessor& accessor, Transport::Accessor& transportAcsr, size_t const channel, CopiedData const& data, juce::Range<double> const& range);
+                ~ActionPaste() override = default;
+
+                // juce::UndoableAction
+                bool perform() override;
+                bool undo() override;
+
+            private:
+                Transport::Accessor& mTransportAccessor;
+                juce::Range<double> const mSavedSelection;
+                juce::Range<double> const mDestinationSelection;
+                Modifier::CopiedData const mSavedData;
+                std::optional<size_t> const mCopyIndex;
+                Modifier::CopiedData const mCopiedData;
+            };
+        } // namespace Modifier
+    }     // namespace Result
+} // namespace Track
+
+ANALYSE_FILE_END
