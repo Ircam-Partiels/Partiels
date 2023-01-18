f428bb63f26cd73862ce67523e1cca0c80a1911a
Pierre Guillot <guillotpierre6@gmail.com>
2023-01-18 10:33:36 +0100

    Scripts: improve publish-all.sh

--- Scripts/publish-all.sh
+++ Scripts/publish-all.sh
@@ -19,7 +19,7 @@ fi
 if [ -z $1 ]; then
 	APP_FULL_VERSION=$APP_VERSION
 else
-  APP_FULL_VERSION=$APP_VERSION-$1
+  	APP_FULL_VERSION=$APP_VERSION-$1
 fi
 
 echo '\033[0;34m' "Preparing " $APP_NAME-v$APP_FULL_VERSION "..."
@@ -27,8 +27,8 @@ echo '\033[0m'
 
 echo '\033[0;34m' "Compressing apple image disk..."
 echo '\033[0m'
-test -f "$APP_NAME-v$APP_FULL_VERSION-MacOS-Universal.zip" && rm "$APP_NAME-v$APP_FULL_VERSION-MacOS-Universal.zip"
-zip -r "$APP_NAME-v$APP_FULL_VERSION-MacOS-Universal.zip" "$APP_NAME-v$APP_VERSION.dmg"
+test -f "$APP_NAME-v$APP_FULL_VERSION-MacOS-64bit-Universal.zip" && rm "$APP_NAME-v$APP_FULL_VERSION-MacOS-64bit-Universal.zip"
+zip -r "$APP_NAME-v$APP_FULL_VERSION-MacOS-64bit-Universal.zip" "$APP_NAME-v$APP_VERSION.dmg"
 
 echo '\033[0;34m' "Downloading Linux artifact..."
 echo '\033[0m'
@@ -40,11 +40,21 @@ echo '\033[0m'
 test -f "$APP_NAME-v$APP_FULL_VERSION-Windows-64bit.zip" && rm "$APP_NAME-v$APP_FULL_VERSION-Windows-64bit.zip"
 curl --output $APP_NAME-v$APP_FULL_VERSION-Windows-64bit.zip --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "$PROJECT_URL/$APP_FULL_VERSION/download?job=Build::Windows"
 
+echo '\033[0;34m' "Downloading documentation artifact..."
+echo '\033[0m'
+test -f "$APP_NAME-v$APP_FULL_VERSION-Manual.zip" && rm "$APP_NAME-v$APP_FULL_VERSION-Manual.zip"
+test -f "$APP_NAME-v$APP_FULL_VERSION-Manual.pdf" && rm "$APP_NAME-v$APP_FULL_VERSION-Manual.pdf"
+curl --output $APP_NAME-v$APP_FULL_VERSION-Manual.zip --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "$PROJECT_URL/$APP_FULL_VERSION/download?job=Doc"
+unzip  $APP_NAME-v$APP_FULL_VERSION-Manual.zip
+mv $APP_NAME-Manual.pdf "$APP_NAME-v$APP_FULL_VERSION-Manual.pdf"
+
 echo '\033[0;34m' "Installing zip files..."
 echo '\033[0m'
 cp "$APP_NAME-v$APP_FULL_VERSION-Linux-64bit.zip" $HOME/Nextcloud/Partiels/Temp
 cp "$APP_NAME-v$APP_FULL_VERSION-Windows-64bit.zip" $HOME/Nextcloud/Partiels/Temp
-cp "$APP_NAME-v$APP_FULL_VERSION-MacOS-Universal.zip" $HOME/Nextcloud/Partiels/Temp
+cp "$APP_NAME-v$APP_FULL_VERSION-MacOS-64bit-Universal.zip" $HOME/Nextcloud/Partiels/Temp
+cp "$APP_NAME-v$APP_FULL_VERSION-Manual.pdf" $HOME/Nextcloud/Partiels/Temp
+cp "$REPO_PATH/BinaryData/Resource/ChangeLog.txt" $HOME/Nextcloud/Partiels/Temp
 
 echo '\033[0;34m' "done"
 echo '\033[0m'
