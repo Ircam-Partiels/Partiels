d3af5a4965c21290478ab2374e6fab2507a9f67d
Pierre Guillot <guillotpierre6@gmail.com>
2023-05-22 11:59:00 +0200

    Scripts: update clang-format-all.sh

--- Scripts/clang-format-all.sh
+++ Scripts/clang-format-all.sh
@@ -5,3 +5,5 @@ cd "$ThisPath/.."
 find Source -iname *.h -o -iname *.cpp | xargs  clang-format -i -style=file
 find Dependencies/Misc/Source -iname *.h -o -iname *.cpp | xargs  clang-format -i -style=file
 find VampPlugins -iname *.h -o -iname *.cpp | xargs  clang-format -i -style=file
+find Dependencies/ircam-vamp-extension/Source -iname *.h -o -iname *.c | xargs  clang-format -i -style=file
+find Dependencies/ircam-vamp-extension/Source -iname *.hpp -o -iname *.cpp | xargs  clang-format -i -style=file
