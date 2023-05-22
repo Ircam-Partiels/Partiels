22fdddf7f0fad37805faa63abb93d96631a8631c
Pierre Guillot <guillotpierre6@gmail.com>
2023-05-22 11:57:03 +0200

    Scripts: format clang-format-all.sh

--- Scripts/clang-format-all.sh
+++ Scripts/clang-format-all.sh
@@ -2,8 +2,8 @@
 
 ThisPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$ThisPath/.."
-find Source -iname *.h -o -iname *.cpp | xargs  clang-format -i -style=file
-find Dependencies/Misc/Source -iname *.h -o -iname *.cpp | xargs  clang-format -i -style=file
-find VampPlugins -iname *.h -o -iname *.cpp | xargs  clang-format -i -style=file
-find Dependencies/ircam-vamp-extension/Source -iname *.h -o -iname *.c | xargs  clang-format -i -style=file
-find Dependencies/ircam-vamp-extension/Source -iname *.hpp -o -iname *.cpp | xargs  clang-format -i -style=file
+find Source -iname *.h -o -iname *.cpp | xargs clang-format -i -style=file
+find Dependencies/Misc/Source -iname *.h -o -iname *.cpp | xargs clang-format -i -style=file
+find VampPlugins -iname *.h -o -iname *.cpp | xargs clang-format -i -style=file
+find Dependencies/ircam-vamp-extension/Source -iname *.h -o -iname *.c | xargs clang-format -i -style=file
+find Dependencies/ircam-vamp-extension/Source -iname *.hpp -o -iname *.cpp | xargs clang-format -i -style=file
