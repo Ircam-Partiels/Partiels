a8370a1b214d6de5dd1f035edd9052742d19260a
Pierre Guillot <guillotpierre6@gmail.com>
2023-11-16 10:07:51 +0100

    Scripts: improve version support in publish-all.sh

--- Scripts/publish-all.sh
+++ Scripts/publish-all.sh
@@ -5,7 +5,7 @@ APP_NAME="Partiels"
 ThisPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_PATH="$ThisPath/.."
 BUILD_PATH="$REPO_PATH/release"
-APP_VERSION=$(defaults read $BUILD_PATH/Partiels.app/Contents/Info.plist CFBundleShortVersionString)
+APP_VERSION=$(<$BUILD_PATH/version.txt)
 
 cd $BUILD_PATH
 
@@ -16,44 +16,39 @@ if [ -z $PRIVATE_TOKEN == ""]; then
   echo '\033[0;31m' "Error: no private token defined"
   exit -1
 fi
-if [ -z $1 ]; then
-  APP_FULL_VERSION=$APP_VERSION
-else
-  APP_FULL_VERSION=$APP_VERSION-$1
-fi
 
-echo '\033[0;34m' "Preparing " $APP_NAME-v$APP_FULL_VERSION "..."
+echo '\033[0;34m' "Preparing " $APP_NAME-v$APP_VERSION "..."
 echo '\033[0m'
 
 echo '\033[0;34m' "Compressing apple image disk..."
 echo '\033[0m'
-test -f "$APP_NAME-v$APP_FULL_VERSION-MacOS-64bit-Universal.zip" && rm "$APP_NAME-v$APP_FULL_VERSION-MacOS-64bit-Universal.zip"
-zip -r "$APP_NAME-v$APP_FULL_VERSION-MacOS-64bit-Universal.zip" "$APP_NAME-v$APP_VERSION.dmg"
+test -f "$APP_NAME-v$APP_VERSION-MacOS-64bit-Universal.zip" && rm "$APP_NAME-v$APP_VERSION-MacOS-64bit-Universal.zip"
+zip -r "$APP_NAME-v$APP_VERSION-MacOS-64bit-Universal.zip" "$APP_NAME-v$APP_VERSION.dmg"
 
 echo '\033[0;34m' "Downloading Linux artifact..."
 echo '\033[0m'
-test -f "$APP_NAME-v$APP_FULL_VERSION-Linux-64bit.zip" && rm "$APP_NAME-v$APP_FULL_VERSION-Linux-64bit.zip"
-curl --output $APP_NAME-v$APP_FULL_VERSION-Linux-64bit.zip --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "$PROJECT_URL/$APP_FULL_VERSION/download?job=Build::Linux"
+test -f "$APP_NAME-v$APP_VERSION-Linux-64bit.zip" && rm "$APP_NAME-v$APP_VERSION-Linux-64bit.zip"
+curl --output $APP_NAME-v$APP_VERSION-Linux-64bit.zip --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "$PROJECT_URL/$APP_VERSION/download?job=Build::Linux"
 
 echo '\033[0;34m' "Downloading windows artifact..."
 echo '\033[0m'
-test -f "$APP_NAME-v$APP_FULL_VERSION-Windows-64bit.zip" && rm "$APP_NAME-v$APP_FULL_VERSION-Windows-64bit.zip"
-curl --output $APP_NAME-v$APP_FULL_VERSION-Windows-64bit.zip --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "$PROJECT_URL/$APP_FULL_VERSION/download?job=Build::Windows"
+test -f "$APP_NAME-v$APP_VERSION-Windows-64bit.zip" && rm "$APP_NAME-v$APP_VERSION-Windows-64bit.zip"
+curl --output $APP_NAME-v$APP_VERSION-Windows-64bit.zip --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "$PROJECT_URL/$APP_VERSION/download?job=Build::Windows"
 
 echo '\033[0;34m' "Downloading documentation artifact..."
 echo '\033[0m'
-test -f "$APP_NAME-v$APP_FULL_VERSION-Manual.zip" && rm "$APP_NAME-v$APP_FULL_VERSION-Manual.zip"
-test -f "$APP_NAME-v$APP_FULL_VERSION-Manual.pdf" && rm "$APP_NAME-v$APP_FULL_VERSION-Manual.pdf"
-curl --output $APP_NAME-v$APP_FULL_VERSION-Manual.zip --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "$PROJECT_URL/$APP_FULL_VERSION/download?job=Doc"
-unzip  $APP_NAME-v$APP_FULL_VERSION-Manual.zip
-mv $APP_NAME-Manual.pdf "$APP_NAME-v$APP_FULL_VERSION-Manual.pdf"
+test -f "$APP_NAME-v$APP_VERSION-Manual.zip" && rm "$APP_NAME-v$APP_VERSION-Manual.zip"
+test -f "$APP_NAME-v$APP_VERSION-Manual.pdf" && rm "$APP_NAME-v$APP_VERSION-Manual.pdf"
+curl --output $APP_NAME-v$APP_VERSION-Manual.zip --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "$PROJECT_URL/$APP_VERSION/download?job=Doc"
+unzip  $APP_NAME-v$APP_VERSION-Manual.zip
+mv $APP_NAME-Manual.pdf "$APP_NAME-v$APP_VERSION-Manual.pdf"
 
 echo '\033[0;34m' "Installing zip files..."
 echo '\033[0m'
-cp "$APP_NAME-v$APP_FULL_VERSION-Linux-64bit.zip" $HOME/Nextcloud/Partiels/Temp
-cp "$APP_NAME-v$APP_FULL_VERSION-Windows-64bit.zip" $HOME/Nextcloud/Partiels/Temp
-cp "$APP_NAME-v$APP_FULL_VERSION-MacOS-64bit-Universal.zip" $HOME/Nextcloud/Partiels/Temp
-cp "$APP_NAME-v$APP_FULL_VERSION-Manual.pdf" $HOME/Nextcloud/Partiels/Temp
+cp "$APP_NAME-v$APP_VERSION-Linux-64bit.zip" $HOME/Nextcloud/Partiels/Temp
+cp "$APP_NAME-v$APP_VERSION-Windows-64bit.zip" $HOME/Nextcloud/Partiels/Temp
+cp "$APP_NAME-v$APP_VERSION-MacOS-64bit-Universal.zip" $HOME/Nextcloud/Partiels/Temp
+cp "$APP_NAME-v$APP_VERSION-Manual.pdf" $HOME/Nextcloud/Partiels/Temp
 cp "$BUILD_PATH/ChangeLog.txt" $HOME/Nextcloud/Partiels/Temp
 
 echo '\033[0;34m' "done"
