2e847e1a603d94baf979fd6769b041ece7e62dce
Pierre Guillot <guillotpierre6@gmail.com>
2024-02-07 11:17:35 +0100

    Track::Plot: remove Overlay class

--- Source/Track/AnlTrackPlot.h
+++ Source/Track/AnlTrackPlot.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "AnlTrackDirector.h"
-#include "AnlTrackRuler.h"
 
 ANALYSE_FILE_BEGIN
 
@@ -11,73 +10,15 @@ namespace Track
     : public juce::Component
     {
     public:
-        Plot(Director& director, Zoom::Accessor& timeZoomAccessor, Transport::Accessor& transportAccessor);
+        Plot(Accessor& accessor, Zoom::Accessor& timeZoomAccessor);
         ~Plot() override;
 
         // juce::Component
         void paint(juce::Graphics& g) override;
 
-        class Overlay
-        : public ComponentSnapshot
-        , public Tooltip::BubbleClient
-        , public juce::SettableTooltipClient
-        {
-        public:
-            Overlay(Plot& plot, juce::ApplicationCommandManager& commandManager);
-            ~Overlay() override;
-
-            // juce::Component
-            void resized() override;
-            void paint(juce::Graphics& g) override;
-            void mouseMove(juce::MouseEvent const& event) override;
-            void mouseEnter(juce::MouseEvent const& event) override;
-            void mouseExit(juce::MouseEvent const& event) override;
-            void mouseDown(juce::MouseEvent const& event) override;
-            void mouseDrag(juce::MouseEvent const& event) override;
-            void mouseUp(juce::MouseEvent const& event) override;
-            void mouseDoubleClick(juce::MouseEvent const& event) override;
-            void mouseMagnify(juce::MouseEvent const& event, float magnifyAmount) override;
-            void mouseWheelMove(juce::MouseEvent const& event, juce::MouseWheelDetails const& wheel) override;
-            void modifierKeysChanged(juce::ModifierKeys const& modifiers) override;
-
-            // ComponentSnapshot
-            void takeSnapshot() override;
-
-        private:
-            // clang-format off
-            enum class ActionMode
-            {
-                  none
-                , create
-                , move
-            };
-            // clang-format on
-
-            void updateActionMode(juce::Point<int> const& point, juce::ModifierKeys const& modifiers);
-            void updateTooltip(juce::Point<int> const& pt);
-
-            Plot& mPlot;
-            Accessor& mAccessor;
-            Zoom::Accessor& mTimeZoomAccessor;
-            Transport::Accessor& mTransportAccessor;
-            juce::ApplicationCommandManager& mCommandManager;
-            Accessor::Listener mListener{typeid(*this).name()};
-            Zoom::Accessor::Listener mTimeZoomListener{typeid(*this).name()};
-            SelectionBar mSelectionBar;
-            ActionMode mActionMode;
-            bool mMouseWasDragged{false};
-            double mMouseDownTime{0.0};
-            Edition mCurrentEdition;
-            ScrollHelper mScrollHelper;
-            juce::ModifierKeys mScrollModifiers;
-            ScrollHelper::Orientation mScrollOrientation{ScrollHelper::Orientation::horizontal};
-        };
-
     private:
-        Director& mDirector;
-        Accessor& mAccessor{mDirector.getAccessor()};
+        Accessor& mAccessor;
         Zoom::Accessor& mTimeZoomAccessor;
-        Transport::Accessor& mTransportAccessor;
         Accessor::Listener mListener{typeid(*this).name()};
         Zoom::Accessor::Listener mZoomListener{typeid(*this).name()};
         Zoom::Grid::Accessor::Listener mGridListener{typeid(*this).name()};
