44ea6c1258c3271d559ada29bba1b61523a034a7
Pierre Guillot <guillotpierre6@gmail.com>
2025-11-17 11:24:04 +0100

    Application: add AcsrType::neuralyzer in the model

--- Source/Application/AnlApplicationModel.h
+++ Source/Application/AnlApplicationModel.h
@@ -3,6 +3,7 @@
 #include "../Document/AnlDocumentExporter.h"
 #include "../Track/AnlTrackModel.h"
 #include "AnlApplicationLookAndFeel.h"
+#include "AnlApplicationNeuralyzerModel.h"
 #include "AnlApplicationOsc.h"
 
 ANALYSE_FILE_BEGIN
@@ -47,6 +48,7 @@ namespace Application
     enum class AcsrType : size_t
     {
           osc
+        , neuralyzer
     };
 
     using AttrContainer = Model::Container
@@ -76,6 +78,7 @@ namespace Application
     
     using AcsrContainer = Model::Container
     < Model::Acsr<AcsrType::osc, Osc::Accessor, Model::Flag::saveable | Model::Flag::notifying, 1>
+    , Model::Acsr<AcsrType::neuralyzer, Neuralyzer::Accessor, Model::Flag::saveable | Model::Flag::notifying, 1>
     >;
     // clang-format on
 
