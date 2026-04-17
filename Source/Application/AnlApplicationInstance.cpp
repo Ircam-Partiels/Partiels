b0d872bf778b4d369dd602c3bf027baa7442d35e
Pierre Guillot <guillotpierre6@gmail.com>
2026-04-17 19:57:45 +0200

    Application::Instance: add Neuralyzer::Agent support

--- Source/Application/AnlApplicationInstance.cpp
+++ Source/Application/AnlApplicationInstance.cpp
@@ -65,7 +65,7 @@ void Application::Instance::initialise(juce::String const& commandLine)
         juce::Process::setDockIconVisible(false);
 #endif
         MiscDebug("Application", "Running as MCP host");
-        auto const result = Mcp::Host::run(mcpPort);
+        auto const result = Neuralyzer::Mcp::Host::run(mcpPort);
         Instance::get().setApplicationReturnValue(result ? 0 : -1);
         Instance::get().systemRequestedQuit();
         return;
@@ -81,6 +81,8 @@ void Application::Instance::initialise(juce::String const& commandLine)
     }
 
     MiscDebug("Application", "Running with GUI");
+
+    Neuralyzer::Agent::initialize();
 
     juce::File::getSpecialLocation(juce::File::SpecialLocationType::userDocumentsDirectory).getChildFile("Ircam").setAsCurrentWorkingDirectory();
 
@@ -108,6 +110,7 @@ void Application::Instance::initialise(juce::String const& commandLine)
     mOscTransportDispatcher = std::make_unique<Osc::TransportDispatcher>(getOscSender());
     mOscMouseDispatcher = std::make_unique<Osc::MouseDispatcher>(getOscSender());
     mNeuralyzerMcpDispatcher = std::make_unique<Neuralyzer::Mcp::Dispatcher>();
+    mNeuralyzerAgent = std::make_unique<Neuralyzer::Agent>(*mNeuralyzerMcpDispatcher.get());
 
     mWindow = std::make_unique<Window>();
     mMainMenuModel = std::make_unique<MainMenuModel>(*mWindow.get());
@@ -375,6 +378,7 @@ void Application::Instance::shutdown()
     mMainMenuModel.reset();
     mWindow.reset();
 
+    mNeuralyzerAgent.reset();
     mNeuralyzerMcpSever.reset();
     mNeuralyzerMcpDispatcher.reset();
     mOscMouseDispatcher.reset();
@@ -396,6 +400,8 @@ void Application::Instance::shutdown()
     mAudioDeviceManager.reset();
     mAudioFormatManager.reset();
     mApplicationCommandManager.reset();
+
+    Neuralyzer::Agent::release();
 
     juce::LookAndFeel::setDefaultLookAndFeel(nullptr);
     mLookAndFeel.reset();
@@ -644,6 +650,11 @@ PluginList::Scanner& Application::Instance::getPluginListScanner()
 Application::Osc::Sender& Application::Instance::getOscSender()
 {
     return *mOscSender.get();
+}
+
+Application::Neuralyzer::Agent& Application::Instance::getNeuralyzerAgent()
+{
+    return *mNeuralyzerAgent.get();
 }
 
 Document::Accessor& Application::Instance::getDocumentAccessor()
