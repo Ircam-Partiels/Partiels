efdfb45973ebbec5d1b7dfdb3a64a97b0ec8efa1
Pierre Guillot <guillotpierre6@gmail.com>
2026-04-28 20:43:50 +0200

    Application::Instance

--- Source/Application/AnlApplicationInstance.h
+++ Source/Application/AnlApplicationInstance.h
@@ -6,7 +6,8 @@
 #include "AnlApplicationLookAndFeel.h"
 #include "AnlApplicationMainMenuModel.h"
 #include "AnlApplicationModel.h"
-#include "AnlApplicationNeuralyzerMcp.h"
+#include "AnlApplicationNeuralyzerBackgroundAgent.h"
+#include "AnlApplicationNeuralyzerRag.h"
 #include "AnlApplicationProperties.h"
 #include "AnlApplicationWindow.h"
 
@@ -46,18 +47,14 @@ namespace Application
         static juce::String getWildCardForAudioFormats();
         static LookAndFeel::ColourChart getColourChart();
 
-        void newDocument();
-        void openDocumentFile(juce::File const& file);
-        void openAudioFiles(std::vector<juce::File> const& files);
-        void openFiles(std::vector<juce::File> const& files);
-
         Accessor& getApplicationAccessor();
         Window* getWindow();
 
         PluginList::Accessor& getPluginListAccessor();
         PluginList::Scanner& getPluginListScanner();
         Osc::Sender& getOscSender();
-        Neuralyzer::Agent& getNeuralyzerAgent();
+        Neuralyzer::BackgroundAgent& getNeuralyzerAgent();
+        Neuralyzer::Rag::Engine& getNeuralyzerRagEngine();
         Document::Accessor& getDocumentAccessor();
         Document::Director& getDocumentDirector();
         Document::FileBased& getDocumentFileBased();
@@ -113,7 +110,8 @@ namespace Application
         std::unique_ptr<Osc::MouseDispatcher> mOscMouseDispatcher;
         std::unique_ptr<Neuralyzer::Mcp::Dispatcher> mNeuralyzerMcpDispatcher;
         std::unique_ptr<Neuralyzer::Mcp::Server> mNeuralyzerMcpSever;
-        std::unique_ptr<Neuralyzer::Agent> mNeuralyzerAgent;
+        std::unique_ptr<Neuralyzer::Rag::Engine> mNeuralyzerRagEngine;
+        std::unique_ptr<Neuralyzer::BackgroundAgent> mNeuralyzerAgent;
 
         std::unique_ptr<Window> mWindow;
         std::unique_ptr<MainMenuModel> mMainMenuModel;
